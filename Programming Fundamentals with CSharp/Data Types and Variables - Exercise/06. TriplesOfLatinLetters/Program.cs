ea9b9a819c8c8b947b3e067aa370272649e77479
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 20:52:44 +0300

    upd program.cs

--- Programming Fundamentals with CSharp/Data Types and Variables - Exercise/06. TriplesOfLatinLetters/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/06. TriplesOfLatinLetters/Program.cs
@@ -6,7 +6,19 @@ namespace _06._TriplesOfLatinLetters
     {
         static void Main(string[] args)
         {
-            
+            int index = 97; //gryph 'a'
+            int firstN = int.Parse(Console.ReadLine());
+            int length = index + firstN; //26;//to gryph 'z'
+            for (int i = index; i < length; i++)
+            {
+                for (int j = index; j < length; j++)
+                {
+                    for (int k = index; k < length; k++)
+                    {
+                        Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                    }
+                }
+            }
         }
     }
 }
