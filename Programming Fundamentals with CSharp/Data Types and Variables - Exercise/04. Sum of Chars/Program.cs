4db1bcddef161d853be64dd28727317e7925cdb6
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 20:24:58 +0300

    update Program.cs

---
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            int charSum = 0;
+            for (int i = 0; i < range; i++)
+            {
+                charSum += (int)char.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The sum equals: {charSum}");
+        }
+    }
+}
