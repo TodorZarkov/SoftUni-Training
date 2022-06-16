1ad0c949d7dcadbddbcbdb96c1d03edf8094acba
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 19:27:01 +0300

    Update Program.cs

--- Programming Fundamentals with CSharp/Data Types and Variables - Exercise/01. Integer Operations/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -6,7 +6,14 @@ namespace _01._Integer_Operations
     {
         static void Main(string[] args)
         {
-            
+            long a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            a += b;
+            a /= c;
+            a *= d;
+            Console.WriteLine(d);
         }
     }
 }
