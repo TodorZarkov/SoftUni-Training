9e72f6fa8ea6593571204f0dfa8c5123c3754c1d
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 20:15:56 +0300

    update Program.cs

--- Programming Fundamentals with CSharp/Data Types and Variables - Exercise/03. Elevator/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -6,7 +6,10 @@ namespace _03._Elevator
     {
         static void Main(string[] args)
         {
-            
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            double courses = Math.Ceiling(numberOfPeople / (double)capacity);
+            Console.WriteLine(courses);
         }
     }
 }
