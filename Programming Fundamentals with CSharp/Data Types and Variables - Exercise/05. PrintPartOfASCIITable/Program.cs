7596d5434f0ebe5d02a870deee8a5edbe41327df
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 20:33:29 +0300

    upd prgogram.cs

---
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/05. PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05._PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startSymbol = int.Parse(Console.ReadLine());
+            int endSymbol = int.Parse(Console.ReadLine());
+
+            for (int i = startSymbol; i <= endSymbol; i++)
+            {
+                char symbol = (char)i;
+                Console.Write($"{symbol} ");
+            }
+        }
+    }
+}
