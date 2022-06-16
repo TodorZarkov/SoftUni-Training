07d88430e0ee26a7dd8fa055951c9108dd58069b
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 21:14:13 +0300

    upd

---
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte lines = byte.Parse(Console.ReadLine());
+            byte capacity = 255;
+            ushort liters = 0;
+            for (int i = 0; i < lines ; i++)
+            {
+                ushort litersPoured = ushort.Parse(Console.ReadLine());
+                liters += litersPoured;
+                if (liters > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    liters -= litersPoured;
+                }
+            }
+            Console.WriteLine(liters);
+
+        }
+    }
+}
