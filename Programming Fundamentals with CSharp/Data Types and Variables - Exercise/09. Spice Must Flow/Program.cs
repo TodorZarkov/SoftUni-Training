0e9daa88617b298af5bb6065656a4c48999bdc5b
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 23:27:01 +0300

    update

--- Programming Fundamentals with CSharp/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -6,7 +6,20 @@ namespace _09._Spice_Must_Flow
     {
         static void Main(string[] args)
         {
-            
+            int initIyeld = int.Parse(Console.ReadLine());
+            long totalIyeld = 0;
+            int minSpices = 100;
+            int expectedSpice = initIyeld;
+            int day = 0;
+            while (expectedSpice >= minSpices)
+            {
+                day++;
+                totalIyeld += expectedSpice - 26;
+                expectedSpice -= 10; 
+            }
+            totalIyeld -= 26;
+            Console.WriteLine(day);
+            Console.WriteLine(totalIyeld);
         }
     }
 }
