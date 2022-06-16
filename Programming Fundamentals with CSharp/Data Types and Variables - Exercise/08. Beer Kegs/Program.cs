5fc3a4cb383f339549047011ffbaa80b05d41220
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-16 22:07:52 +0300

    new task

---
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte inputLength = byte.Parse(Console.ReadLine());
+            double kegVolume = 0;
+            string kegName = string.Empty;
+
+            for (byte i = 0; i < inputLength; i++)
+            {
+
+                string curName = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double curVolume = Math.PI*Math.Pow(radius, 2)*height;
+                if (curVolume>kegVolume)
+                {
+                    kegVolume = curVolume;
+                    kegName = curName;
+                }
+            }
+            Console.WriteLine(kegName);
+
+        }
+    }
+}
---
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
