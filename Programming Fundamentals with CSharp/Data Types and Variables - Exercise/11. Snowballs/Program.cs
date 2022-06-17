9be707a4aaaaba4089e7ec07aafb1ffc1d622953
todor <t.p.zarkov@gmail.com>
2022-06-17 23:38:03 +0300

    upd

--- Programming Fundamentals with CSharp/Data Types and Variables - Exercise/11. Snowballs/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -6,7 +6,24 @@ namespace _11._Snowballs
     {
         static void Main(string[] args)
         {
-            
+            int balls = int.Parse(Console.ReadLine());
+            int value = 0;
+            int snow = 0;
+            int time = 0;
+            int quality = 0;
+            for (int i = 0; i < balls; i++)
+            {
+                snow = int.Parse(Console.ReadLine());
+                time = int.Parse(Console.ReadLine());
+                quality = int.Parse(Console.ReadLine());
+
+                int curValue = (snow / time) ^ quality;
+                if (curValue > value)
+                {
+                    value = curValue;
+                }
+            }
+            Console.WriteLine($"{snow} : {time} = {value} ({quality})");
         }
     }
 }
