64a0d02428bd28b522931f6da7c5c4e872f370c6
todor <t.p.zarkov@gmail.com>
2022-06-17 23:01:43 +0300

    update

--- Programming Fundamentals with CSharp/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -6,10 +6,26 @@ namespace _10._Poke_Mon
     {
         static void Main(string[] args)
         {
-            int pokePower = int.Parse(Console.ReadLine());
-            int distanceBetweenTargets = int.Parse(Console.ReadLine());
-            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int pokePower = int.Parse(Console.ReadLine());//N
+            int initPower = pokePower;
+            int distanceBetweenTargets = int.Parse(Console.ReadLine());//M
+            int exhaustionFactor = int.Parse(Console.ReadLine());//Y
             int pokedTargets = 0;
+            while (pokePower >= distanceBetweenTargets)
+            {
+                pokePower -= distanceBetweenTargets;
+                pokedTargets++;
+                if (initPower/pokePower==2&&initPower%pokePower==0)
+                {
+                    if (pokePower/exhaustionFactor != 0)
+                    {
+                        pokePower /= exhaustionFactor;
+                    }
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokedTargets);
+
             
         }
     }
