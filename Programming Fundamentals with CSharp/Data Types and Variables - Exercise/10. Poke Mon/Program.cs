f7a5884a2706d9d4b993cc06b5d7a1898793d026
todor <t.p.zarkov@gmail.com>
2022-06-17 22:38:46 +0300

    upd

--- Programming Fundamentals with CSharp/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -8,8 +8,12 @@ namespace _10._Poke_Mon
         {
             int pokePower = int.Parse(Console.ReadLine());
             int distanceBetweenTargets = int.Parse(Console.ReadLine());
-            int exhaustionFactor = int.Parse(Console.ReadLine());
-            int pokedTargets = 0;
+            //int exhaustionFactor = int.Parse(Console.ReadLine());
+            //int pokedTargets = 0;
+            for (int i = 0; i < 5; i++)
+            {
+
+            }
         }
     }
 }
