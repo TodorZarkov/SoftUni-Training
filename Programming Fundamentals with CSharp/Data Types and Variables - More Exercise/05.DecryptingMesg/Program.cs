087d90f4f188d1b9d70cee6602585db6babd3daa
todor <t.p.zarkov@gmail.com>
2022-06-18 17:42:44 +0300

    final update task 05

--- Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/05.DecryptingMesg/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/05.DecryptingMesg/Program.cs
@@ -6,7 +6,16 @@ namespace _05.DecryptingMesg
     {
         static void Main(string[] args)
         {
-            
+            int key = int.Parse(Console.ReadLine());
+            int lines = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+
+            for (int i = 0; i < lines; i++)
+            {
+                char gryph = char.Parse(Console.ReadLine());
+                message += (char)((int)gryph + key);
+            }
+            Console.WriteLine(message);
         }
     }
 }
