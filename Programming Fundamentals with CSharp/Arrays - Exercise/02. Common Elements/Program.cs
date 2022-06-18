1bf2c291ae69c5dd67b9d33c87d1aedca51d865c
todor <t.p.zarkov@gmail.com>
2022-06-18 23:31:36 +0300

    final update 02

---
+++ Programming Fundamentals with CSharp/Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char separator = ' ';
+            string[] firstLine = Console.ReadLine().Split(separator);
+            string[] secondLine = Console.ReadLine().Split(separator);
+            string output = string.Empty;
+            for (int i = 0; i < secondLine.Length; i++)
+            {
+                for (int j = 0; j < firstLine.Length; j++)
+                {
+                    if (secondLine[i] == firstLine[j])
+                    {
+                        output +=  firstLine[j] + " ";
+                    }
+                }
+            }
+            Console.WriteLine(output.TrimEnd());
+        }
+    }
+}
