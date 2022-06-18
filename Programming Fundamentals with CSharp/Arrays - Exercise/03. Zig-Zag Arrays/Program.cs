819539972e69dd25ffb904d215eca1235011738b
todor <t.p.zarkov@gmail.com>
2022-06-19 00:43:23 +0300

    init task 03

---
+++ Programming Fundamentals with CSharp/Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char separator = ' ';
+            int lines = int.Parse(Console.ReadLine());
+            string[] firstLineOut = new string[lines];
+            string[] secondLineOut = new string[lines];
+            string[] line = new string[2];
+
+            for (int i = 0; i < lines; i++)
+            {
+                line = Console.ReadLine().Split(separator);
+                firstLineOut[i] = line[i % 2];
+                secondLineOut[i] = line[(i+1) % 2 ];
+            }
+            Console.WriteLine(String.Join(separator,firstLineOut));
+            Console.WriteLine(String.Join(separator,secondLineOut));
+
+        }
+    }
+}
