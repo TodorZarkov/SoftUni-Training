363ea1023959b768426b6958948dfbae9d1f2c80
todor <t.p.zarkov@gmail.com>
2022-06-18 13:17:08 +0300

    final upd

---
+++ Programming Fundamentals with CSharp/Data Types and Variables - Exercise/12. Snowballs II/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int balls = int.Parse(Console.ReadLine());
+            double value = 0;
+            int snow = 0;
+            int time = 1;
+            int quality = 0;
+            for (int i = 0; i < balls; i++)
+            {
+                int curSnow = int.Parse(Console.ReadLine());
+                int curTime = int.Parse(Console.ReadLine());
+                int curQuality = int.Parse(Console.ReadLine());
+
+                int temp = curSnow / curTime;
+                //(snowballSnow / snowballTime) ^ snowballQuality
+                double curValue = Math.Pow((double)temp, (double)curQuality);//here we got wrong result
+
+
+                if (curValue >= value)
+                {
+                    value = curValue;
+                    snow = curSnow;
+                    time = curTime;
+                    quality = curQuality;
+
+                }
+            }
+            Console.WriteLine($"{snow} : {time} = {value:f0} ({quality})");
+        }
+    }
+}
