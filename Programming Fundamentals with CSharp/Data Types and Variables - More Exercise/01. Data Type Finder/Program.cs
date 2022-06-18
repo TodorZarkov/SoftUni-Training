e58fd937ee8851549bf4748158e7245fcf5d8d31
todor <t.p.zarkov@gmail.com>
2022-06-18 14:11:15 +0300

    update

--- Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -6,7 +6,33 @@ namespace _01._Data_Type_Finder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+
+                if (int.TryParse(input, out int inputInt))
+                {
+                    Console.WriteLine($"{inputInt} is integer type");
+                }
+                else if (double.TryParse(input, out double inputFloat))
+                {
+                    Console.WriteLine($"{inputFloat} is floating point type");
+                }
+                else if (char.TryParse(input, out char inputChar))
+                {
+                    Console.WriteLine($"{inputChar} is character type");
+                }
+                else if (bool.TryParse(input, out bool inputBool))
+                {
+                    Console.WriteLine($"{inputBool} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
