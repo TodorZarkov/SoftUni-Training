aaf4d30c21d07247105bb1403e4827beb4214603
todor <t.p.zarkov@gmail.com>
2022-06-18 22:04:01 +0300

    final update 01

--- Programming Fundamentals with CSharp/Arrays - Exercise/01. Train/Program.cs
+++ Programming Fundamentals with CSharp/Arrays - Exercise/01. Train/Program.cs
@@ -6,7 +6,20 @@ namespace _01._Train
     {
         static void Main(string[] args)
         {
-            
+            int lines = int.Parse(Console.ReadLine());
+            int[] passangers = new int[lines];
+            for (int i = 0; i < lines; i++)
+            {
+                passangers[i] = int.Parse(Console.ReadLine());
+            }
+            int totalPassangers = 0;
+            foreach (int item in passangers)
+            {
+                Console.Write(item + " ");
+                totalPassangers += item;
+            }
+            Console.WriteLine();
+            Console.WriteLine(totalPassangers);
         }
     }
 }
