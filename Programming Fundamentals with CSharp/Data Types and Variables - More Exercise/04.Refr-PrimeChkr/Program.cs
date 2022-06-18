13d7fb08c5a54b04b4e795c570f045895848af15
todor <t.p.zarkov@gmail.com>
2022-06-18 16:25:19 +0300

    new task 04

---
+++ Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/04.Refr-PrimeChkr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.Refr_PrimeChkr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
