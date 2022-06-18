8f301cca92d04627364a8dfedf7491bf72f2d9c5
todor <t.p.zarkov@gmail.com>
2022-06-18 20:06:59 +0300

    final update task 06

--- Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/06.BalancedBracks/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/06.BalancedBracks/Program.cs
@@ -6,7 +6,36 @@ namespace _06.BalancedBracks
     {
         static void Main(string[] args)
         {
-            
+            int lines = int.Parse(Console.ReadLine());
+            int opened = 0;
+            int closed = 0;
+
+
+            for (int i = 0; i < lines; i++)
+            {
+                string inputLine = Console.ReadLine();
+                if (inputLine == "(")
+                {
+                    opened++;
+                }
+                else if (inputLine == ")")
+                {
+                    closed++;
+                }
+                if ((int)Math.Abs(opened - closed) > 1 || opened - closed < 0)
+                {
+                    Console.WriteLine("UNBALANCED");
+                    return;
+                }
+            }
+            if (opened - closed == 0)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
         }
     }
 }
