26c37f5f12a4764a63b518048b8952e7ab020fd4
todor <t.p.zarkov@gmail.com>
2022-06-18 16:27:45 +0300

    to refract

--- Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/04.Refr-PrimeChkr/Program.cs
+++ Programming Fundamentals with CSharp/Data Types and Variables - More Exercise/04.Refr-PrimeChkr/Program.cs
@@ -6,7 +6,35 @@ namespace _04.Refr_PrimeChkr
     {
         static void Main(string[] args)
         {
-            
+            int ___Do___ = int.Parse(Console.ReadLine());
+
+            for (int takoa = 2; takoa <= ___Do___; takoa++)
+
+            {
+
+                bool takovalie = true;
+
+                for (int cepitel = 2; cepitel < takoa; cepitel++)
+
+                {
+
+                    if (takoa % cepitel == 0)
+
+                    {
+
+                        takovalie = false;
+
+                        break;
+
+                    }
+
+                }
+
+                Console.WriteLine("{0} -> {1}", takoa, takovalie);
+
+            }
+
+
         }
     }
 }
