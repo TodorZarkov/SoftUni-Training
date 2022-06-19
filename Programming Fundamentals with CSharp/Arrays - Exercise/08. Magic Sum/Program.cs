1385f4e5ca995bcc694f0c1f0019a327bde9a8eb
todor <t.p.zarkov@gmail.com>
2022-06-19 18:23:35 +0300

    init task 08

---
+++ Programming Fundamentals with CSharp/Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char separator = ' ';
+            int[] arr = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+            int targetSum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == targetSum)
+                    {
+                        Console.WriteLine($"{arr[i]} {arr[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
