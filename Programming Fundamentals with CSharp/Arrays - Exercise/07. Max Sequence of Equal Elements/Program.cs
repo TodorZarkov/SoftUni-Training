e4d335a5e8486f572cac2fc69ba08133c7b3f5cf
todor <t.p.zarkov@gmail.com>
2022-06-19 17:35:05 +0300

    init task 07

---
+++ Programming Fundamentals with CSharp/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char separator = ' ';
+            int[] arr = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+            int sequenceLen = 1;
+            int startIndex = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int temSeqLen = 1;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        temSeqLen++;
+                        if (temSeqLen > sequenceLen)
+                        {
+                            sequenceLen = temSeqLen;
+                            startIndex = i;
+                        }
+                    }
+                    else
+                    {
+                        i = j - 1;
+                        break;
+                    }
+                    
+                }
+
+
+            }
+            for (int i = startIndex; i < startIndex+sequenceLen; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
