f54c0ab919b352517e9dbc821eecf7e5fac9335c
todor <t.p.zarkov@gmail.com>
2022-06-19 15:25:04 +0300

    init task 05

---
+++ Programming Fundamentals with CSharp/Arrays - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char separator = ' ';
+            int[] arr = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+            bool isTop = true;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                isTop = true;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] <= arr[j])
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+                if (isTop)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
