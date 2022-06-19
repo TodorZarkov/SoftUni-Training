585dd1b12d46a8ed6795b704a3abd4903d0cd2c9
todor <t.p.zarkov@gmail.com>
2022-06-19 14:35:25 +0300

    final update 04

--- Programming Fundamentals with CSharp/Arrays - Exercise/04. Array Rotation/Program.cs
+++ Programming Fundamentals with CSharp/Arrays - Exercise/04. Array Rotation/Program.cs
@@ -10,13 +10,19 @@ namespace _04._Array_Rotation
             string[] intNumbers = Console.ReadLine().Split(separator);
             int offset = int.Parse(Console.ReadLine());
             string[] rotated = new string[intNumbers.Length];
+
+            while(offset >= intNumbers.Length)
+            {
+                offset -= intNumbers.Length;
+            }
+
             for (int i = 0; i < intNumbers.Length - offset; i++)
             {
                 rotated[i] = intNumbers[i + offset];
             }
             for (int i = intNumbers.Length - offset; i < intNumbers.Length; i++)
             {
-                rotated[i] = intNumbers[i - (offset + 1)];
+                rotated[i] = intNumbers[i - (intNumbers.Length - offset)];
             }
             Console.WriteLine(string.Join(separator,rotated));
         }
