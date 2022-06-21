e29f1536e28286decd6ffe66f7fa630ecda61b1e
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-22 01:08:32 +0300

    init 10

--- Programming Fundamentals with CSharp/Methods - Exercise/10. Top Number/Program.cs
+++ Programming Fundamentals with CSharp/Methods - Exercise/10. Top Number/Program.cs
@@ -6,7 +6,37 @@ namespace _10._Top_Number
     {
         static void Main(string[] args)
         {
-             
+             int endNumber = int.Parse(Console.ReadLine());
+            for (int i = 1; i < endNumber; i++)
+            {
+                if (IsSumDiv(i.ToString()) && ContainsOdd(i.ToString()))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        
+        //is sum of the digits divisible by 8
+        static bool IsSumDiv(string num)
+        {
+            int sum = 0;
+            //the digits in ascii are from 48(0) to 57(9)
+            foreach (char digit in num)
+            {
+                sum += (int)(digit - 48);
+            }
+            return sum%8 == 0;
+        }
+
+        static bool ContainsOdd(string num)
+        {
+            foreach (char digit in num)
+            {
+                //the digits in ascii are from 48(0) to 57(9)
+                if (digit % 2 != 0)
+                    return true;
+            }
+            return false;
         }
     }
 }
