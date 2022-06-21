ef9ab113421c759a049c7ae768fe0a46739a3403
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-22 00:14:14 +0300

    init task 09

---
+++ Programming Fundamentals with CSharp/Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                Console.WriteLine(IsPalindrom(input).ToString().ToLower());
+                input = Console.ReadLine();
+            }
+        }
+        static bool IsPalindrom (string line)
+        {
+            char[] frw= line.ToCharArray();
+            char[] bcw = new char[frw.Length];
+            frw.CopyTo(bcw, 0);
+            Array.Reverse(bcw);   
+            
+            return string.Join(' ',frw) == string.Join(' ',bcw);
+        }
+    }
+}
