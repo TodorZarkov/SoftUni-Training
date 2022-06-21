a4c9603d913faaa10119f4f1c51c6dbb4f48cac2
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-21 23:18:04 +0300

    final upd 08

--- Programming Fundamentals with CSharp/Methods - Exercise/08. Factorial Division/Program.cs
+++ Programming Fundamentals with CSharp/Methods - Exercise/08. Factorial Division/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace _08._Factorial_Division
 {
@@ -6,7 +7,25 @@ namespace _08._Factorial_Division
     {
         static void Main(string[] args)
         {
-             
+
+            BigInteger a = int.Parse(Console.ReadLine());
+            BigInteger b = int.Parse(Console.ReadLine());
+            //Calculate the factorial of each number.
+            //Divide the first result by the second and
+            //print the result of the division formatted to the second decimal point.
+            Console.WriteLine($"{Division(Factorial(a), Factorial(b)):f2}");
+        }
+        static decimal Division(BigInteger a, BigInteger b)
+        {
+            return (decimal)a / (decimal)b;
+        }
+        static BigInteger Factorial(BigInteger a)
+        {
+            if (a == 1)
+            {
+                return a;
+            }
+            return a * Factorial(a - 1);
         }
     }
 }
