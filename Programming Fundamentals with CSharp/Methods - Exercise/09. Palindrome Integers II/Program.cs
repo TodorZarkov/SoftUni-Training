d12f3f382e9a97bf7cddf7b49253dcd54b58d611
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-22 00:39:34 +0300

    init palindrome variant

---
+++ Programming Fundamentals with CSharp/Methods - Exercise/09. Palindrome Integers II/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                Console.WriteLine(IsPalindrom(input).ToString().ToLower());
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool IsPalindrom(string line)
+        {
+            int j = line.Length - 1;
+            for (int i = 0; i < line.Length/2; i++)
+            {
+                if (line[i] != line[j-i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
