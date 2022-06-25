fbc623be3a8024460668a82a3621fcdb9d8a1b4f
todor <t.p.zarkov@gmail.com>
2022-06-25 11:47:46 +0300

    init 03

--- Programming Fundamentals with CSharp/Lists - Exercise/03. House Party/Program.cs
+++ Programming Fundamentals with CSharp/Lists - Exercise/03. House Party/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._House_Party
 {
@@ -6,7 +8,26 @@ namespace _03._House_Party
     {
         static void Main(string[] args)
         {
-            
+            int lines = int.Parse(Console.ReadLine());
+            List<string> names = new();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ').ToArray();
+                if (line[2] == "going!")
+                {
+                    if (!names.Contains(line[0])) names.Add(line[0]);
+                    else Console.WriteLine($"{line[0]} is already in the list!");
+                }
+                else if (line[2] == "not")
+                {
+                    if (!names.Remove(line[0])) Console.WriteLine($"{line[0]} is not in the list!");
+                }
+            }
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
         }
     }
 }
