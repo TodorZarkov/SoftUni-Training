be13c39d9692d185cc5846101ef1dcd49e357893
todor <t.p.zarkov@gmail.com>
2022-06-25 15:39:00 +0300

    init 07

---
+++ Programming Fundamentals with CSharp/Lists - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strLst = Console.ReadLine().Split('|').ToList();
+            strLst.ForEach(x => x.Trim());
+            List<int[]> intList = new List<int[]>();
+            foreach (string item in strLst)
+            {
+                intList.Add(item.Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            }   
+            intList.Reverse();
+            List<int> numbers = new List<int>();
+            foreach (int[] arrInt in intList)
+            {
+                foreach (int number in arrInt)
+                {
+                    numbers.Add(number);
+                }
+            }
+            Console.WriteLine(String.Join(' ',numbers));
+            
+
+        }
+    }
+}
