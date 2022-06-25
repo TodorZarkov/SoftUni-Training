75726e94b96124617c5b6316f17424498f3d26d5
todor <t.p.zarkov@gmail.com>
2022-06-25 11:52:32 +0300

    update 03

--- Programming Fundamentals with CSharp/Lists - Exercise/03. House Party/Program.cs
+++ Programming Fundamentals with CSharp/Lists - Exercise/03. House Party/Program.cs
@@ -9,18 +9,21 @@ namespace _03._House_Party
         static void Main(string[] args)
         {
             int lines = int.Parse(Console.ReadLine());
-            List<string> names = new();
+            List<string> names = new List<string>();
             for (int i = 0; i < lines; i++)
             {
                 string[] line = Console.ReadLine().Split(' ').ToArray();
                 if (line[2] == "going!")
                 {
-                    if (!names.Contains(line[0])) names.Add(line[0]);
-                    else Console.WriteLine($"{line[0]} is already in the list!");
+                    if (!names.Contains(line[0])) 
+                        names.Add(line[0]);
+                    else 
+                        Console.WriteLine($"{line[0]} is already in the list!");
                 }
                 else if (line[2] == "not")
                 {
-                    if (!names.Remove(line[0])) Console.WriteLine($"{line[0]} is not in the list!");
+                    if (!names.Remove(line[0])) 
+                        Console.WriteLine($"{line[0]} is not in the list!");
                 }
             }
             foreach (string name in names)
