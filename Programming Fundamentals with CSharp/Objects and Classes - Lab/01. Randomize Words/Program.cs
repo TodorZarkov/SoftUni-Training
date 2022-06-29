4d8b5df7eede9ca5792e9c3059265c7cc70f06f8
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-29 23:06:09 +0300

    update 01

--- Programming Fundamentals with CSharp/Objects and Classes - Lab/01. Randomize Words/Program.cs
+++ Programming Fundamentals with CSharp/Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -6,7 +6,17 @@ namespace _01._Randomize_Words
     {
         static void Main(string[] args)
         {
-            
+            string[] line = Console.ReadLine().Split(' ');
+            Random rand = new Random();
+            for (int i = 0; i < line.Length; i++)
+            {
+                string temp = line[i];
+                int randIndex = rand.Next(0, line.Length);
+                line[i] = line[randIndex];
+                line[randIndex] = temp;
+            }
+            Console.WriteLine(string.Join(' ', line));
+
         }
     }
 }
