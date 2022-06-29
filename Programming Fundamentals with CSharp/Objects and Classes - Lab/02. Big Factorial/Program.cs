0b2c5ba91f98e5a12024799b469cebf4fb9d692b
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-06-29 23:09:28 +0300

    last update  02

---
+++ Programming Fundamentals with CSharp/Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+
+        }
+        static BigInteger Factorial(BigInteger n)
+        {
+            if (n == 1) return n;
+            return Factorial(n - 1) * n;
+        }
+    }
+}
