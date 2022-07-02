46f00af514889bbd8a21bb94f8078d20b71efa2b
todor <t.p.zarkov@gmail.com>
2022-07-02 23:49:34 +0300

    init 06

---
+++ Programming Fundamentals with CSharp/Objects and Classes - Lab/06. Store Boxes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string line = Console.ReadLine();
+        List<Box> boxes = new List<Box>();
+        while (line != "end")
+        {
+            string[] data = line.Split(' ').ToArray();
+            //"{Serial Number} {Item Name} {Item Quantity} {itemPrice}"
+            //The Price of one box has to be calculated: itemQuantity * itemPrice.
+
+            Box box = new Box { SerialNumber = int.Parse(data[0]), ItemQuantity = int.Parse(data[2]) };
+            box.Item.Name = data[1];
+            box.Item.Price = int.Parse(data[3]);
+            boxes.Add(box);
+        }
+
+    }
+}
+
+class Item
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+}
+
+class Box
+{
+    public Box()//constructor//
+    {
+        Item = new Item();
+    }
+    public Item Item { get; set; }
+    public int SerialNumber { get; set; }
+    public int ItemQuantity { get; set; }
+    public double Price()
+    {
+        return this.Item.Price * this.ItemQuantity;
+    }
+
+}
