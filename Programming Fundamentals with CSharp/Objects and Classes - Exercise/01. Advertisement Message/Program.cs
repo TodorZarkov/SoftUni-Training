3cb1ad6ada041038c0bea001da205f2352ced436
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-04 00:56:47 +0300

    init 02

--- Programming Fundamentals with CSharp/Objects and Classes - Exercise/01. Advertisement Message/Program.cs
+++ Programming Fundamentals with CSharp/Objects and Classes - Exercise/01. Advertisement Message/Program.cs
@@ -6,7 +6,33 @@ namespace _01._Advertisement_Message
     {
         static void Main(string[] args)
         {
-            
+            string[] phrases =
+                {
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can't live without this product."
+            };
+            string[] events =
+                {
+                "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!"
+            };
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+            int lines = int.Parse(Console.ReadLine());
+            //The format of the output message is the following: "{phrase} {event} {author} – {city}."
+            for (int i = 0; i < lines; i++)
+            {
+                Random rnd = new Random();
+                Console.WriteLine($"{phrases[rnd.Next(phrases.Length)]} {events[rnd.Next(events.Length)]} {authors[rnd.Next(authors.Length)]} – {cities[rnd.Next(cities.Length)]}.");
+            }
         }
     }
 }
