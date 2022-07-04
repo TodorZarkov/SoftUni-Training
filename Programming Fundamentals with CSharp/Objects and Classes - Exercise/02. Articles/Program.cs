85fbc05650c043a9bcdfc43152595ee1fd1d52c6
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-05 01:41:00 +0300

    init 02 Articles

--- Programming Fundamentals with CSharp/Objects and Classes - Exercise/02. Articles/Program.cs
+++ Programming Fundamentals with CSharp/Objects and Classes - Exercise/02. Articles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Articles
 {
@@ -6,7 +8,48 @@ namespace _02._Articles
     {
         static void Main(string[] args)
         {
-           
+            string[] line = Console.ReadLine().Split(", ").ToArray();
+            Article article = new Article(line[0], line[1], line[2]);
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ", 2).ToArray();
+                if (command[0] == "Edit")
+                    article.Edit(command[1]);
+                else if (command[0] == "ChangeAuthor")
+                    article.ChangeAuthor(command[1]);
+                else if (command[0] == "Rename")
+                    article.Rename(command[1]);
+            }
+            Console.WriteLine(article.ToString());
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public void Edit(string newContent)
+        {
+            this.Content = newContent;
+        }
+        public void ChangeAuthor(string newAuthor)
+        {
+            this.Author = newAuthor;
+        }
+        public void Rename(string newTitle)
+        {
+            this.Title = newTitle;
+        }
+        override public string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
         }
     }
 }
