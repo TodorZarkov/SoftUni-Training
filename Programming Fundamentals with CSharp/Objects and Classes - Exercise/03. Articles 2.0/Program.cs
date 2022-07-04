a9b7df59dbdd538e8a24ad2d96c749092648ceba
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-05 01:54:32 +0300

    init _03._Articles_2._0

---
+++ Programming Fundamentals with CSharp/Objects and Classes - Exercise/03. Articles 2.0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Articles_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] line = Console.ReadLine().Split(", ").ToArray();
+                Article article = new Article(line[0], line[1], line[2]);
+                articles.Add(article);
+            }
+            foreach (Article article  in articles)
+            {
+                Console.WriteLine(article.ToString());
+            }
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        
+        override public string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
+        }
+    }
+}
