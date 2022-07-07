2cc8bda6ee5bea92ac8a3c4e3c30b110bb4894a2
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-07 23:36:42 +0300

    init 05

--- Programming Fundamentals with CSharp/Objects and Classes - Exercise/05. Teamwork Projects/Program.cs
+++ Programming Fundamentals with CSharp/Objects and Classes - Exercise/05. Teamwork Projects/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Teamwork_Projects
 {
@@ -6,7 +8,23 @@ namespace _05._Teamwork_Projects
     {
         static void Main(string[] args)
         {
-            
+            int countOfTeams = int.Parse(Console.ReadLine());
+
         }
+    }
+
+    class Team
+    {
+        Team(string name, string teamCreator)
+        {
+            this.Name = name;
+            this.Creator = teamCreator;
+            List<string> members = new List<string>();
+            this.Members = members;
+        }
+        public string Name { get; }
+        public string Creator { get; }
+        public List<string> Members { get; set; }
+
     }
 }
