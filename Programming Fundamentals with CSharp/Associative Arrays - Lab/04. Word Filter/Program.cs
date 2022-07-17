f6830961aa7ebb83c58a4f1111974f43feb97060
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 21:34:11 +0300

    init  04. Word Filter

---
+++ Programming Fundamentals with CSharp/Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(' ').Where(w => w.Length % 2 == 0).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
