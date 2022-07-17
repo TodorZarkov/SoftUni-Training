61923783808e7a13ce85638c1488c38a7cd179e7
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 21:18:30 +0300

    init 03. Word Synonyms

---
+++ Programming Fundamentals with CSharp/Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> wordsSynonyms = new Dictionary<string, List<string>>();
+            int numberOfPairs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPairs; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+                if (!wordsSynonyms.ContainsKey(word))
+                {
+                    wordsSynonyms[word] = new List<string> { synonim };
+                }
+                else
+                {
+                    wordsSynonyms[word].Add(synonim);
+                }
+            }
+
+            foreach (var kvp in wordsSynonyms)
+            {
+                Console.WriteLine($"{kvp.Key} - {string.Join(", ", kvp.Value)}");
+            }
+        }
+    }
+}
