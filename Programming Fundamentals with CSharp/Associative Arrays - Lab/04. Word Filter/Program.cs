56e59161c734ccf9232bccc5f29cc73cfde9221c
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 21:36:03 +0300

    update 04._Word_Filter

--- Programming Fundamentals with CSharp/Associative Arrays - Lab/04. Word Filter/Program.cs
+++ Programming Fundamentals with CSharp/Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -8,7 +8,11 @@ namespace _04._Word_Filter
     {
         static void Main(string[] args)
         {
-            Console.ReadLine().Split(' ').Where(w => w.Length % 2 == 0).ToList().ForEach(Console.WriteLine);
+            Console.ReadLine()
+                .Split(' ')
+                .Where(w => w.Length % 2 == 0)
+                .ToList()
+                .ForEach(Console.WriteLine);
         }
     }
 }
