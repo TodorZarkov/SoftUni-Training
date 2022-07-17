a04f9bd881053444b755b732e21a46541239d641
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 23:19:44 +0300

    update 01._Count_Chars_in_a_String

--- Programming Fundamentals with CSharp/Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
+++ Programming Fundamentals with CSharp/Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Count_Chars_in_a_String
 {
@@ -6,7 +8,18 @@ namespace _01._Count_Chars_in_a_String
     {
         static void Main(string[] args)
         {
-           
+            List<char> symbols = Console.ReadLine().Where(w => w != ' ').ToList();
+            Dictionary<char, int> occurences = new Dictionary<char, int>();
+            foreach (char symbol in symbols)
+            {
+                if (!occurences.ContainsKey(symbol))
+                {
+                    occurences[symbol] = 0;
+                }
+                occurences[symbol]++;
+            }
+
+            occurences.ToList().ForEach(kvp => Console.WriteLine($"{kvp.Key} -> {kvp.Value}"));
         }
     }
 }
