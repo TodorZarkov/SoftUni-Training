7506adf2b29979cf31d1d8273b2ce4e874671485
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 23:22:52 +0300

    init 02._A_Miner_Task

---
+++ Programming Fundamentals with CSharp/Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
