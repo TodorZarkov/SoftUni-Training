cd3cf5765fc91634ff6c74cbf807423e0fb8d5dd
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 20:16:58 +0300

    init 02._Odd_Occurrences

---
+++ Programming Fundamentals with CSharp/Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split(' ').Select(w => w.ToLower()).ToList();
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (!frequency.ContainsKey(word))
+                {
+                    frequency[word] = 0;
+                }
+                frequency[word]++;
+            }
+            foreach (var kvp in frequency.Where(k => k.Value % 2 != 0))
+            {
+                Console.Write($"{kvp.Key} ");
+            }
+            
+        }
+    }
+}
