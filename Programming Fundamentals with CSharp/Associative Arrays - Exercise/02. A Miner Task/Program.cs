e338e3a2fe38f81928a3db314fee99e2df89a8e8
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 23:53:09 +0300

    update 02. A Miner Task

--- Programming Fundamentals with CSharp/Associative Arrays - Exercise/02. A Miner Task/Program.cs
+++ Programming Fundamentals with CSharp/Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -8,7 +8,32 @@ namespace _02._A_Miner_Task
     {
         static void Main(string[] args)
         {
-            
+            Dictionary<string, int> mining = new Dictionary<string, int>();
+            bool isOddLine = true;
+            string line = Console.ReadLine();
+            string resource = string.Empty;
+            int quantity = 0;
+            while (line != "stop")
+            {
+                if (isOddLine)
+                {
+                    resource = line;
+                    isOddLine = false;
+                }
+                else
+                {
+                    quantity = int.Parse(line);
+                    if (!mining.ContainsKey(resource))
+                    {
+                        mining[resource] = 0;
+                    }
+                    mining[resource] += quantity;
+                    isOddLine = true;
+                }
+
+                line = Console.ReadLine();
+            }
+            mining.ToList().ForEach(kvp => Console.WriteLine($"{kvp.Key} -> {kvp.Value}"));
         }
     }
 }
