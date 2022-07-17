1da12c900140d02dc770bb747b7eef2af8e049f0
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-17 19:27:18 +0300

    init 01. Count Real Numbers

--- Programming Fundamentals with CSharp/Associative Arrays - Lab/01. Count Real Numbers/Program.cs
+++ Programming Fundamentals with CSharp/Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Count_Real_Numbers
 {
@@ -6,7 +8,21 @@ namespace _01._Count_Real_Numbers
     {
         static void Main(string[] args)
         {
-            
+            List<int> nums = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToList();
+            SortedDictionary<int, int> frequency = new SortedDictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!frequency.ContainsKey(num))
+                {
+                    frequency[num] = 0;
+                }
+                frequency[num]++;
+            }
+
+            foreach (var kvp in frequency)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
         }
     }
 }
