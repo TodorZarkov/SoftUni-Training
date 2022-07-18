4734236f2278a479a7aaaa84460cfdca5a32de4c
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-18 19:37:39 +0300

    init 03._Orders

--- Programming Fundamentals with CSharp/Associative Arrays - Exercise/03. Orders/Program.cs
+++ Programming Fundamentals with CSharp/Associative Arrays - Exercise/03. Orders/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._Orders
 {
@@ -6,7 +8,40 @@ namespace _03._Orders
     {
         static void Main(string[] args)
         {
-            
+            string line = Console.ReadLine();
+            Dictionary<string, PriceAndQuantity> products = new Dictionary<string, PriceAndQuantity>();
+            while (line != "buy")
+            {
+                string[] productData = line.Split(' ').ToArray();
+                string productName = productData[0];
+                double currentPrice = double.Parse(productData[1]);
+                int currentQuantity = int.Parse(productData[2]);
+
+                PriceAndQuantity pq = new PriceAndQuantity(currentPrice,currentQuantity);
+                if (!products.ContainsKey(productName))
+                {
+                    products[productName] = pq;
+                }
+                else
+                {
+                    pq.quantity += products[productName].quantity;
+                    products[productName] = pq;
+                }
+
+                line = Console.ReadLine();
+            }
+            products.ToList().ForEach(kvp => Console.WriteLine($"{kvp.Key} -> {kvp.Value.price*kvp.Value.quantity:f2}"));
+        }
+
+    }
+    struct PriceAndQuantity
+    {
+        public double price;
+        public int quantity;
+        public PriceAndQuantity(double price, int quantity)
+        {
+            this.price = price;
+            this.quantity = quantity;
         }
     }
 }
