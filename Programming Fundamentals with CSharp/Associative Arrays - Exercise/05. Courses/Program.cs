23354179d073bef443d301616b22540a2acea71f
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-20 07:11:45 +0300

    init 05._Courses

---
+++ Programming Fundamentals with CSharp/Associative Arrays - Exercise/05. Courses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+            string line = Console.ReadLine();
+            while (line != "end")
+            {
+                string[] commands = line.Split(" : ");
+                string courseName = commands[0];
+                string registeredStudent = commands[1];
+                if (!courses.ContainsKey(courseName))
+                {
+                    List<string> students = new List<string>();
+                    students.Add(registeredStudent);
+                    courses[courseName] = students;
+
+                }
+                else
+                    courses[courseName].Add(registeredStudent);
+                line = Console.ReadLine();
+            }
+            courses.ToList().ForEach(kvp => Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}{string.Join($"{Environment.NewLine}-- ", kvp.Value)}"));
+        }
+    }
+}
