b060a25b2649008a8bf9776c92f4224db819c858
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-21 19:00:24 +0300

    init 07. Company Users

---
+++ Programming Fundamentals with CSharp/Associative Arrays - Exercise/07. Company Users/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyEmploees = new Dictionary<string, List<string>>();
+            string line = Console.ReadLine();
+            while (line != "End")
+            {
+                string[] companyEmpleeData = line.Split(" -> ");
+                string company = companyEmpleeData[0];
+                string emploeeID = companyEmpleeData[1];
+                if (companyEmploees.ContainsKey(company))
+                {
+                    if (!companyEmploees[company].Contains(emploeeID))
+                        companyEmploees[company].Add(emploeeID);
+                }
+                else
+                {
+                    companyEmploees[company] = new List<string> { emploeeID };
+                }
+
+                line = Console.ReadLine();
+            }
+            foreach (var kvp in companyEmploees)
+            {
+                Console.WriteLine($"{kvp.Key}");
+                foreach (string empID in kvp.Value)
+                {
+                    Console.WriteLine($"-- {empID}");
+                }
+            }
+        }
+    }
+}
