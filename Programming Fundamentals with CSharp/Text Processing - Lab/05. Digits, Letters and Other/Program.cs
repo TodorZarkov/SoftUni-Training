0e5bb2624f4960282654b02821d98c201c3227e1
todor <t.p.zarkov@gmail.com>
2022-07-24 10:06:47 +0300

    init 05._Digits__Letters_and_Other

---
+++ Programming Fundamentals with CSharp/Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder otherSymbols = new StringBuilder();
+            foreach (char symbol in text)
+            {
+                if (symbol >= '0' && symbol <= '9')
+                {
+                    digits.Append(symbol);
+                }
+                else if ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z'))
+                {
+                    letters.Append(symbol);
+                }
+                else
+                {
+                    otherSymbols.Append(symbol);
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(otherSymbols);
+
+        }
+    }
+}
