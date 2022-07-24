a1e322c7f3de24a8fcfb8d9aec73a311df0db9ad
todor <t.p.zarkov@gmail.com>
2022-07-24 16:19:24 +0300

    init 03._Extract_File

--- Programming Fundamentals with CSharp/Text Processing - Exercise/03. Extract File/Program.cs
+++ Programming Fundamentals with CSharp/Text Processing - Exercise/03. Extract File/Program.cs
@@ -6,7 +6,14 @@ namespace _03._Extract_File
     {
         static void Main(string[] args)
         {
-           
+            string path = Console.ReadLine();
+            int extensionFirstIndex = path.LastIndexOf('.') + 1;
+            int extensionCountSymbols = path.Length - extensionFirstIndex;
+            int fileStartIndex = path.LastIndexOf('\\') + 1;
+            int fileCountSymbols = extensionFirstIndex - fileStartIndex - 1;
+            Console.WriteLine($"File name: {path.Substring(fileStartIndex, fileCountSymbols)}");
+            Console.WriteLine($"File extension: {path.Substring(extensionFirstIndex, extensionCountSymbols)}");
+
         }
     }
 }
