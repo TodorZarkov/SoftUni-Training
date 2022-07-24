99675ced7ea62160e126de948f78cd13212bc2d7
todor <t.p.zarkov@gmail.com>
2022-07-24 08:56:17 +0300

    init 02. Repeat Strings

---
+++ Programming Fundamentals with CSharp/Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            string repeatedWords = string.Empty;
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    repeatedWords += word;
+                }
+            }
+            Console.WriteLine(repeatedWords);
+        }
+    }
+}
