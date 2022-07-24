53dd9eaad1beabfd56db7d3ed27c48b6e6ac2599
todor <t.p.zarkov@gmail.com>
2022-07-24 08:34:33 +0300

    init 01._Reverse_Strings

---
+++ Programming Fundamentals with CSharp/Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = Console.ReadLine();
+            while (inputWord != "end")
+            {
+                char[] outputWord = inputWord.ToCharArray();
+                Array.Reverse(outputWord);
+                Console.Write($"{inputWord} = ");
+                Console.WriteLine(outputWord);
+
+
+                inputWord = Console.ReadLine();
+            }
+        }
+    }
+}
