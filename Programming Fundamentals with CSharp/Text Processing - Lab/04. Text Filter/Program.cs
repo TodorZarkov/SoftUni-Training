a89f1486ec10d9cb0eea81c3da3a8e626e245067
todor <t.p.zarkov@gmail.com>
2022-07-24 09:36:23 +0300

    init 04._Text_Filter

---
+++ Programming Fundamentals with CSharp/Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banList = Console.ReadLine().Split(", ");
+            StringBuilder text = new StringBuilder(Console.ReadLine());
+            foreach (string banWord in banList)
+            {
+                string asterisk = new string('*', banWord.Length);
+                text.Replace(banWord, asterisk);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
