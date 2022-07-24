7b4656d3e16214ada47ec72881db4d824649f7ad
todor <t.p.zarkov@gmail.com>
2022-07-24 14:51:19 +0300

    init 01._Valid_Usernames

---
+++ Programming Fundamentals with CSharp/Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(", ");
+            foreach (string name in names)
+            {
+                bool isValidName = true;
+                if (name.Length >= 3 && name.Length <= 16)
+                {
+                    foreach (char symbol in name)
+                    {
+                        if (!Char.IsLetterOrDigit(symbol) && !(symbol == '-') && !(symbol == '_'))
+                        {
+                            isValidName = false;
+                            break;
+                        }
+                    }
+                    if (isValidName)
+                        Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
