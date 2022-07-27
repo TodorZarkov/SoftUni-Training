e1c0ef5a97ba7f9534804a77aea17ccb2449d0ed
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-27 21:46:09 +0300

    init 07._String_Explosion

---
+++ Programming Fundamentals with CSharp/Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder explodedText = new StringBuilder(Console.ReadLine());
+            string text = explodedText.ToString();
+        }
+    }
+}
