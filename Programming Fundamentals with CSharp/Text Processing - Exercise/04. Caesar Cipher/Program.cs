c8a64540658c81daa9cc2b410b348cdaa463338f
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-27 19:08:44 +0300

    init 04._Caesar_Cipher

---
+++ Programming Fundamentals with CSharp/Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder encriptedText = new StringBuilder();
+            const int offset = 3;
+            for (int i = 0; i < text.Length; i++)
+            {
+                encriptedText.Append((char)((int)text[i] + offset));
+            }
+            Console.WriteLine(encriptedText);
+        }
+    }
+}
