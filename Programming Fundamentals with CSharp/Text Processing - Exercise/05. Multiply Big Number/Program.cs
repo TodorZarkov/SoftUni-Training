e03a8914f711739fbadb1df5d73d7aa25b7fc1f8
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-07-27 20:51:45 +0300

    init 05._Multiply_Big_Number

---
+++ Programming Fundamentals with CSharp/Text Processing - Exercise/05. Multiply Big Number/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            // {
+
+
+
+            string inputNumber = Console.ReadLine();
+            byte digit = byte.Parse(Console.ReadLine());
+            if (digit == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            byte[] bigNumber = new byte[inputNumber.Length + 1];
+            int strIndex = inputNumber.Length - 1;
+            byte reminder = 0;
+            for (int i = 0; i < bigNumber.Length; i++)
+            {
+                if (strIndex >= 0)
+                {
+                    bigNumber[i] = (byte)((reminder + (inputNumber[strIndex] - 48) * digit) % 10);
+                    reminder = (byte)((reminder + (inputNumber[strIndex] - 48) * digit) / 10);
+                    strIndex--;
+                }
+                else
+                {
+                    bigNumber[i] = reminder;
+                }
+            }
+            int len = bigNumber.Length;
+            if (bigNumber.Last() == 0)
+            {
+                len = bigNumber.Length - 1;
+            }
+            for (int i = len - 1; i >= 0; i--)
+            {
+                Console.Write(bigNumber[i]);
+            }
+        }
+    }
+}
+//}
