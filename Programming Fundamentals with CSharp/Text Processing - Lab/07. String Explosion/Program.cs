46255ddcd26ed482dd470b2e140b81bf96b33679
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-01 23:54:30 +0300

    update 07._String_Explosion

--- Programming Fundamentals with CSharp/Text Processing - Lab/07. String Explosion/Program.cs
+++ Programming Fundamentals with CSharp/Text Processing - Lab/07. String Explosion/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _07._String_Explosion
 {
@@ -6,7 +7,30 @@ namespace _07._String_Explosion
     {
         static void Main(string[] args)
         {
-            
+            StringBuilder explodedText = new StringBuilder();
+            string text = Console.ReadLine();
+            const char bomb = '>';
+            int bombIndex = text.IndexOf(bomb);
+            int magnitude = (int)text[bombIndex + 1] - 48;
+            AddCharsFromString(explodedText, text, 0, bombIndex);
+            while (bombIndex != -1)
+            {
+                int nextBombIndex = text.IndexOf(bomb);
+                while (nextBombIndex > bombIndex)
+                {
+                    bombIndex = nextBombIndex;
+                    explodedText.Append(bomb);
+                    nextBombIndex = text.IndexOf(bomb);
+                }
+                bombIndex = text.IndexOf(bomb);
+            }
+        }
+        static void AddCharsFromString(StringBuilder sb, string text, int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                sb.Append(text[i]);
+            }
         }
     }
 }
