50f1dcc241cd5aa0f30a05652de518ccf45f0521
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-03 20:55:02 +0300

    init 01._Match_Full_Name

---
+++ Programming Fundamentals with CSharp/Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rx = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string names = Console.ReadLine();
+            MatchCollection matchedNames = Regex.Matches(names, rx);
+
+            foreach ( Match name in matchedNames)
+            {
+                Console.Write(name.Value + " ");
+            }
+        }
+    }
+}
