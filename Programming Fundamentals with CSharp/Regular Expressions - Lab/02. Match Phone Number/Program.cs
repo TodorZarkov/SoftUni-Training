7fdfd7322ea5ed551151c145735b3b05af0ed9ba
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-03 22:38:19 +0300

    init 02._Match_Phone_Number

---
+++ Programming Fundamentals with CSharp/Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rx = @"\+359 2 [0-9]{3,3} [0-9]{4,4}\b|\+359-2-[0-9]{3,3}-[0-9]{4,4}\b";
+            string numbers = Console.ReadLine();
+            MatchCollection matchedNumbers = Regex.Matches(numbers, rx);
+
+            Console.WriteLine(string.Join(", ", matchedNumbers)); 
+            
+        }
+    }
+}
