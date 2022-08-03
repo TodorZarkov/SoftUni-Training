5a5dcf5c7eef6733b47190f737b27628ddb3f3ec
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-03 23:24:21 +0300

    init 03._Match_Dates

---
+++ Programming Fundamentals with CSharp/Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex rx = new Regex(@"\b(?<day>\d{2})([.\-/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
+            string dates = Console.ReadLine();
+            MatchCollection validDates = rx.Matches(dates);
+            foreach ( Match date in validDates)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
