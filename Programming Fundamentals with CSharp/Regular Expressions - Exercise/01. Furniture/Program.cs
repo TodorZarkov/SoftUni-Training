e8a50552e9caa28f858f062ebce8a4fe0b98d858
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-06 20:21:08 +0300

    init 01._Furniture

--- Programming Fundamentals with CSharp/Regular Expressions - Exercise/01. Furniture/Program.cs
+++ Programming Fundamentals with CSharp/Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace _01._Furniture
 {
@@ -7,14 +8,29 @@ namespace _01._Furniture
     {
         static void Main(string[] args)
         {
+            double total = 0;
+            List<string> furniture = new List<string>();
             Regex rx = new Regex(@"(^>>(?<name>\w+)<<(?<price>\d+\.{0,1}\d+)!(?<quantity>\d+)\b)");
             string line = Console.ReadLine();
             while (line != "Purchase")
             {
-                MatchCollection matchedLines = rx.Matches(line);
+                Match matchedLine = rx.Match(line);
+                if (!rx.IsMatch(line))
+                {
+                    line = Console.ReadLine();
+                    continue;
+                }
+                string name = matchedLine.Groups["name"].Value;
+                furniture.Add(name);
+                int quantity = int.Parse(matchedLine.Groups["quantity"].Value);
+                double price = double.Parse(matchedLine.Groups["price"].Value);
+                total = total + price * quantity;
 
                 line = Console.ReadLine();
             }
+            Console.WriteLine("Bought furniture:");
+            furniture.ForEach(f => Console.WriteLine(f));
+            Console.WriteLine($"Total money spend: {total:f2}");
         }
     }
 }
