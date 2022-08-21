0c735001ade00d2f7b4bd332735e01de5458065c
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-21 18:47:27 +0300

    init 12._Refactor_Special_Numbers

---
+++ Programming Fundamentals with CSharp/Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            for (int number = 1; number <= range; number++)
+            {
+                int tempNumber = number;
+                int sumOfDigits = 0;
+                while (number > 0)
+                {
+                    sumOfDigits += number % 10;
+                    number = number / 10;
+                }
+                bool isSpecial = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine("{0} -> {1}", tempNumber, isSpecial);
+                number = tempNumber;
+            }
+        }
+    }
+}
