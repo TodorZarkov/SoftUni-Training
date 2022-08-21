3e1bdf74a79147d59a3d9de96d46fe3ddcdbe6dc
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-21 21:11:03 +0300

    init 02._Print_Numbers_in_Reverse_Order

---
+++ Programming Fundamentals with CSharp/Arrays - Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbersReversed = new int[n];
+            int reverseCounter = n;
+            for (int i = 0; i < n; i++)
+            {
+                reverseCounter--;
+                numbersReversed[reverseCounter] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine(String.Join(' ',numbersReversed));
+        }
+    }
+}
