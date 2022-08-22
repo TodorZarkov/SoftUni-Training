ab616461a309edfd52329996a2cae7b41e47e1a5
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-22 21:24:00 +0300

    init 04._Reverse_Array_of_Strings

---
+++ Programming Fundamentals with CSharp/Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(' ', Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse()));
+        }
+    }
+}
