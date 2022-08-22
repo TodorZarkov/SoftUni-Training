78717dc29b64dbbbf2b096912840bc73236dfb53
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-08-22 21:31:55 +0300

    init 05._Sum_Even_Numbers

---
+++ Programming Fundamentals with CSharp/Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Where(x => x % 2 == 0).Sum());
+        }
+    }
+}
