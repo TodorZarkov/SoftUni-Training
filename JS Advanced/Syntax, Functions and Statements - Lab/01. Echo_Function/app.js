595b917c72f97f64f7004ff645e964f042300304
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-14 23:12:22 +0300

    js-advanced init

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/01. Echo_Function/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let number = 10;
+let anotherNumber = 234.34324234;
+console.log(number + anotherNumber);
+
+let objkt = {
+    name: 'sharo',
+    breed: 'ksh'
+}
+
+console.log(objkt.name);
+objkt.color = 'blue';
+console.log(objkt.color);
