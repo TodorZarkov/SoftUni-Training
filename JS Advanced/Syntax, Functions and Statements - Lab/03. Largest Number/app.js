dd29f12080c08e515499af633541035b9c70e8bb
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-15 10:21:04 +0300

    final 03. Largest Number

--- JS Advanced/Syntax, Functions and Statements - Lab/03. Largest Number/app.js
+++ JS Advanced/Syntax, Functions and Statements - Lab/03. Largest Number/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-console.log('Hello from 03');
+function largestNumber(num1, num2, num3){
+    let maxNumber = Math.max(num1, num2, num3)
+    console.log(`The largest number is ${maxNumber}.`)
+}
+
+largestNumber(-3, -5, -22.5);
