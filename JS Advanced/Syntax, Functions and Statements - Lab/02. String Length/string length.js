e791c05f2faf2468033cda57f80a8421ccef89a0
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-15 10:05:25 +0300

    final 02. String Length

--- JS Advanced/Syntax, Functions and Statements - Lab/02. String Length/string length.js
+++ JS Advanced/Syntax, Functions and Statements - Lab/02. String Length/string length.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('Hello world');
+function calcStrings(...params){
+    let lenSum = 0;
+    for (let i = 0; i < params.length; i++) {
+        lenSum += params[i].length;
+    }
+    let lenAverage = Math.round(lenSum/params.length);
+    console.log(lenSum);
+    console.log(lenAverage);
+}
+calcStrings('pasta', '5', '22.3');
