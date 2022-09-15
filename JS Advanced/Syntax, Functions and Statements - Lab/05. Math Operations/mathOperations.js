9a9a66fd693b778eb0798e6e0769f6bb6fc84d63
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-15 11:12:08 +0300

    init  05. Math Operations

--- JS Advanced/Syntax, Functions and Statements - Lab/05. Math Operations/mathOperations.js
+++ JS Advanced/Syntax, Functions and Statements - Lab/05. Math Operations/mathOperations.js
@@ -1,2 +1,17 @@
 'use strict';
 
+function mathOperation(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+        default:
+            return;
+    }
+    console.log(result);
+}
+mathOperation(3, 5.5, '*')
