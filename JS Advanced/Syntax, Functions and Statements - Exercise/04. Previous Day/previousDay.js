e0ce88b7caafaf3bdbd0989e6adf60b2e6ab3dc3
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-18 07:44:09 +0300

    init 04. Previous Day

---
+++ JS Advanced/Syntax, Functions and Statements - Exercise/04. Previous Day/previousDay.js
@@ -0,0 +1,10 @@
+'use strict';
+//input y,m,d
+function prevDayOf(year,month,day){
+    let date = new Date(year,month-1,day-1);
+    let newYear = date.getFullYear();
+    let newMonth = date.getMonth()+1;
+    let newDay = date.getDate();
+    console.log(`${newYear}-${newMonth}-${newDay}`)
+}
+prevDayOf(2016, 9, 30);
