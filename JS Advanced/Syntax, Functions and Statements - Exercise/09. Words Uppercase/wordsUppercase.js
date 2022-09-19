1946929eebbbd3eabbdc89b687c3cc806e7c2f01
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-20 02:08:08 +0300

    init 09. Words Uppercase

---
+++ JS Advanced/Syntax, Functions and Statements - Exercise/09. Words Uppercase/wordsUppercase.js
@@ -0,0 +1,13 @@
+'use strict';
+function parsWordsToUpper(text) {
+    let txt = String(text);
+    const regEx = /\w+/g;
+    let matches = [...text.matchAll(regEx)];
+    let result = '';
+    matches.forEach(x => result += `${String(x[0]).toUpperCase()}, `);
+    result = result.trim();
+    result = result.substring(0, result.length - 1);
+    console.log(result);
+}
+
+parsWordsToUpper('   232ff FSD__');
