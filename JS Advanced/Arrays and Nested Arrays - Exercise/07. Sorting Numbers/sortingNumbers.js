1d7115e2434fc90700b2e54c11b8b3ba6d907bfa
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-22 00:15:25 +0300

    update 07. Sorting Numbers

--- JS Advanced/Arrays and Nested Arrays - Exercise/07. Sorting Numbers/sortingNumbers.js
+++ JS Advanced/Arrays and Nested Arrays - Exercise/07. Sorting Numbers/sortingNumbers.js
@@ -1,12 +1,18 @@
 'use strict';
 function solve(arr) {
-    let arrAsc = arr.sort((a, b) => a - b);
-    let arrDes = arr.sort((a, b) => b - a);
+    let arrAsc = [];
+    let arrDes = [];
+
+    arrAsc = arr.sort((a, b) => a - b).map(x => x);
+    arrDes = arr.reverse().map(x => x);
     let result = [];
     for (let i = 0; i < arr.length / 2; i++) {
         result.push(arrAsc[i]);
-        result.push(arrDes[i]);
+        if (result.length < arr.length)
+            result.push(arrDes[i]);
 
     }
+    //console.log(result);
     return result;
 }
+solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56, 20]);
