d3bdf9265eac1d71bccaa11ec7dbd07c16c731dd
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-21 11:49:59 +0300

    init 01. Even Position Elements

--- JS Advanced/Arrays and Nested Arrays - Lab/01. Even Position Elements/evenPositionElements.js
+++ JS Advanced/Arrays and Nested Arrays - Lab/01. Even Position Elements/evenPositionElements.js
@@ -1,1 +1,10 @@
 'use strict';
+function solve(arr) {
+    let result = '';
+    for (let i = 0; i < arr.length; i += 2) {
+        result += (`${arr[i]}` + ' ');
+    }
+    result.trimEnd();
+    console.log(result);
+}
+solve(['20', '30', '40', '50', '60'])
