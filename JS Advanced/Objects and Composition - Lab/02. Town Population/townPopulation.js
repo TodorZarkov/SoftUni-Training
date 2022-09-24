436ab5c0a9c5efb2b95d1d65795b3e65329ccfe8
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-24 23:14:28 +0300

    init 02. Town Population

---
+++ JS Advanced/Objects and Composition - Lab/02. Town Population/townPopulation.js
@@ -0,0 +1,21 @@
+'use strict';
+function solve(inputTowns) {
+    let towns = {};
+    for (let townAsString of inputTowns) {
+        let [name, population] = townAsString.split(' <-> ');
+        population = Number(population);
+        if (towns[name] === undefined) {
+            towns[name] = population;
+        } else {
+            towns[name] += population;
+        }
+    }
+    for (let town in towns) {
+        console.log(`${town} : ${towns[town]}`)
+    }
+}
+solve(['Istanbul <-> 100000',
+    'Honk Kong <-> 2100004',
+    'Jerusalem <-> 2352344',
+    'Mexico City <-> 23401925',
+    'Istanbul <-> 1000']);
