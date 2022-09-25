d70f98ccb32e73e08d7cfca97cc2e99130ef1f65
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-25 10:07:29 +0300

    init 03. City Taxes

--- JS Advanced/Objects and Composition - Lab/03. City Taxes/cityTaxes.js
+++ JS Advanced/Objects and Composition - Lab/03. City Taxes/cityTaxes.js
@@ -1,1 +1,22 @@
 'use strict';
+function solve(name, population, treasury) {
+    return {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percentage){
+            this.population = Math.floor((this.population*(1+percentage/100)));
+        },
+        applyRecession(percentage){
+            this.treasury = Math.floor(this.treasury*(1-percentage/100));
+        }
+    };
+}
+
+solve('Tortuga',
+    7000,
+    15000)
