f1f3c24ee9500ff475c02fd7458189ec9238aee3
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-26 19:07:58 +0300

    update 01. Calorie Object

---
+++ JS Advanced/Objects and Composition - Exercise/03. Car Factory/carFactory.js
@@ -0,0 +1,16 @@
+'use strict';
+function solve(requ = {}) {
+    let car = {
+        model: requ.model,
+        engine: {
+            power: 0,
+            volume: 0
+        },
+        carriage: {
+            type: '',
+            color: ''
+        },
+        wheels: [0, 0, 0, 0]
+    }
+
+}
