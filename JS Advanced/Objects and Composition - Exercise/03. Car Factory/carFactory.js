ce9426389acf058b79a7c3f5e6349c4d33da5471
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-26 19:58:59 +0300

    update 03. Car Factory

--- JS Advanced/Objects and Composition - Exercise/03. Car Factory/carFactory.js
+++ JS Advanced/Objects and Composition - Exercise/03. Car Factory/carFactory.js
@@ -1,7 +1,7 @@
 'use strict';
-function solve(requ = {}) {
+function solve(requirements = {}) {
     let car = {
-        model: requ.model,
+        model: '',
         engine: {
             power: 0,
             volume: 0
@@ -13,4 +13,36 @@ function solve(requ = {}) {
         wheels: [0, 0, 0, 0]
     }
 
+    car.model = requirements.model;
+    if (requirements.power <= 90) {
+        car.engine.power = 90;
+        car.engine.volume = 1800;
+    } else if (requirements.power <= 120) {
+        car.engine.power = 120;
+        car.engine.volume = 2400;
+    } else {
+        car.engine.power = 200;
+        car.engine.volume = 3500;
+    }
+
+    car.carriage.color = requirements.color;
+    car.carriage.type = requirements.carriage;
+
+    let availWheelSize = requirements.wheelsize;
+    if (availWheelSize % 2 === 0) {
+        availWheelSize-- ;
+    }
+    car.wheels.fill(availWheelSize);
+    
+    console.log(car.carriage);
+    return car;
 }
+
+solve({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+});
+
