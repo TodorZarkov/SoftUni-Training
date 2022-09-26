d4f69372476e8eb9d6d5f0f832b17a36c424829b
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-27 02:48:48 +0300

    init 05. Lowest Prices in Cities

--- JS Advanced/Objects and Composition - Exercise/05. Lowest Prices in Cities/lowestPrices.js
+++ JS Advanced/Objects and Composition - Exercise/05. Lowest Prices in Cities/lowestPrices.js
@@ -8,12 +8,18 @@ function solve(input = []) {
         town.product = {};
         town.product.name = townData[1];
         town.product.price = Number(townData[2]);
-        town.entry = i;
+        town.entry = 0;
+        for (let t of towns) {
+            if (t.product.name === town.product.name)
+                town.entry = t.entry;
+            else
+                town.entry = i;
+        }
         towns.push(town);
     }
     towns.sort((b, a) => {
         let result = a.product.price - b.product.price;
-        if(result === 0 && a.product.name === b.product.name){
+        if (result === 0 && a.product.name === b.product.name) {
             result = a.entry - b.entry;
         }
         return result;
@@ -21,23 +27,26 @@ function solve(input = []) {
         return c.product.name.localeCompare(d.product.name);
     })
     let result = [];
-    for (let i = 0; i < towns.length-1; i++) {
-        if (towns[i].product.name !== towns[i+1].product.name){
+    for (let i = 0; i < towns.length - 1; i++) {
+        if (towns[i].product.name !== towns[i + 1].product.name) {
             result.push(towns[i]);
         }
     }
-    result.push(towns[towns.length-1]);
-    result.sort((a,b)=>a.entry - b.entry);
+    result.push(towns[towns.length - 1]);
+    result.sort((a, b) => a.entry - b.entry);
 
-    for(let town of result){
+    for (let town of result) {
         console.log(`${town.product.name} -> ${town.product.price} (${town.name})`)
     }
 }
 
-solve(['Sample Town | Sample Product | 1000',
-'Sample Town | Orange | 2',
-'Sample Town | Peach | 1',
-'Sofia | Orange | 3',
-'Sofia | Peach | 2',
-'New York | Sample Product | 1000.1',
-'New York | Burger | 10']);
+solve(['Sofia City | Audi | 100000',
+'Sofia City | BMW | 100000',
+'Sofia City | Mitsubishi | 10000',
+'Sofia City | Mercedes | 10000',
+'Sofia City | NoOffenseToCarLovers | 0',
+'Mexico City | Audi | 1000',
+'Mexico City | BMW | 99999',
+'Mexico City | Mitsubishi | 10000',
+'New York City | Mitsubishi | 1000',
+'Washington City | Mercedes | 1000']);
