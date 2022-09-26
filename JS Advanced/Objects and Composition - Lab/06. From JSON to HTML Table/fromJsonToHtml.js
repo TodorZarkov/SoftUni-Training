6fdd8720534464360fa1ace04ee51f8b380139a0
TodorZarkov <t.p.zarkov@gmail.com>
2022-09-26 11:36:15 +0300

    update 06. From JSON to HTML Table

--- JS Advanced/Objects and Composition - Lab/06. From JSON to HTML Table/fromJsonToHtml.js
+++ JS Advanced/Objects and Composition - Lab/06. From JSON to HTML Table/fromJsonToHtml.js
@@ -1,1 +1,33 @@
 'use strict';
+function fromJSONToHTMLTable(input) {
+    //Write your code here
+    let students = JSON.parse(input);
+    let tableRow = [];
+    let result = `<table>\n`;
+
+    tableRow.push('\t<tr>');
+    for (let key in students[0]) {
+        tableRow.push(`<th>${key}</th>`);
+    }
+    tableRow.push('</tr>\n');
+
+    for (let student of students) {
+        tableRow.push('\t<tr>');
+        for (let key in student) {
+            tableRow.push(`<td>${student[key]}</td>`);
+
+        }
+        tableRow.push('</tr>\n');
+    }
+    result += tableRow.join('') + `</table>`;
+    console.log(result);
+
+//     return (`<table>
+// ${result}
+// </table>`);
+}
+
+fromJSONToHTMLTable(`[{"Name":"Stamat",
+"Score":5.5},
+{"Name":"Rumen",
+"Score":6}]`)
