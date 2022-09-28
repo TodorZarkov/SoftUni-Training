c555a226311709a27d3a2fffe92d95d2c8374bfe
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-28 09:42:05 +0300

    init 01._Basic_Stack_Operations

---
+++ C# Advanced/Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int pushes = data[0];
+            int pops = data[1];
+            int toCheck = data[2];
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < pushes; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+            for (int i = 0; i < pops; i++)
+            {
+                stack.Pop();
+            }
+            bool isPresent = stack.Contains(toCheck);
+            if (isPresent)
+            {
+                Console.WriteLine(isPresent);
+            }
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Queues - Lab/1. Reverse a String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
