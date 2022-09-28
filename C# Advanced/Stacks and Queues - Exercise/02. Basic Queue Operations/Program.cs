c300f115401604169df36229be1ee04f6ed0cf99
TodorZarkov <t.p.zarkov@gmail.com>
2022-09-28 16:41:39 +0300

    init 02. Basic Queue Operations

---
+++ C# Advanced/Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int enqueues = data[0];
+            int dequeues = data[1];
+            int toCheck = data[2];
+
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < enqueues; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+            for (int i = 0; i < dequeues; i++)
+            {
+                queue.Dequeue();
+            }
+            bool isPresent = queue.Contains(toCheck);
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (isPresent)
+            {
+                Console.WriteLine(isPresent.ToString().ToLower());
+            }
+            else
+            {
+                List<int> list = queue.ToList();
+                list.Sort();
+                Console.WriteLine(list.First());
+            }
+        }
+    }
+}
