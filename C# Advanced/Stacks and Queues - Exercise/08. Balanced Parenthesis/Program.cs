eda8c9bb41b7a16906a0d62642d4378eba537165
TodorZarkov <t.p.zarkov@gmail.com>
2022-09-29 15:40:51 +0300

    init 08. Balanced Parenthesis

---
+++ C# Advanced/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string brackets = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            bool isBalanced = true;
+            foreach (char bracket in brackets)
+            {
+                if(bracket == '(' || bracket == '{' || bracket == '[')
+                {
+                    stack.Push(bracket);
+                }
+                else if (bracket == ')' || bracket == '}' || bracket == ']')
+                {
+                    if (stack.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    char opBracket = stack.Pop();
+                    bool isTheSameClosing = bracket - opBracket > 0 && bracket - opBracket <= 2 ? true : false;
+                    if (!isTheSameClosing)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(isBalanced?"YES":"NO");
+        }
+    }
+}
