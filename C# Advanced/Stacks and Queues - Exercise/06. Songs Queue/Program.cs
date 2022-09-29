c2bc4998b54a5f76c7ede7174dab53a7e5e8ecee
TodorZarkov <t.p.zarkov@gmail.com>
2022-09-29 12:38:59 +0300

    init 06. Songs Queue

---
+++ C# Advanced/Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>();
+            Console.ReadLine().Split(", ").ToList().ForEach(x => songs.Enqueue(x));
+            while (songs.Count > 0)
+            {
+                string[] commands = Console.ReadLine().Split(' ', 2);
+                switch (commands[0])
+                {
+                    case "Play":
+                        songs.Dequeue();
+                        break;
+                    case "Add":
+                        if (!songs.Contains(commands[1]))
+                        {
+                            songs.Enqueue(commands[1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{commands[1]} is already contained!");
+                        }
+                        break;
+                    case "Show":
+                        Console.WriteLine(String.Join(", ",songs));
+                        break;
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
