1bfebbfba9d562daae81327a88ebc8f3d0a2a024
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-30 09:12:56 +0300

    init 10._Crossroads

--- C# Advanced/Stacks and Queues - Exercise/10. Crossroads/Program.cs
+++ C# Advanced/Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _10._Crossroads
 {
@@ -6,7 +8,64 @@ namespace _10._Crossroads
     {
         static void Main(string[] args)
         {
-            
+            int greenDuration = int.Parse(Console.ReadLine());
+            int yellowDuration = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            string line = Console.ReadLine();
+            int carsPassed = 0;
+            while (line != "END")
+            {
+                if (line == "green")
+                {
+                    carsPassed += queue.Count;
+                    int crossing = Crossing(greenDuration, yellowDuration, queue);
+                    if(crossing != 0)
+                    {
+                        Console.WriteLine("A crash happened!");
+                        Console.WriteLine($"{queue.Peek()} was hit at {(char)crossing}.");
+                        return;
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(line);
+                }
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carsPassed} total cars passed the crossroads.");
+        }
+
+
+
+
+
+        static int Crossing(int greenDuration, int yellowDuration, Queue<string> queue)
+        {
+            string car = "";
+            while (greenDuration > 0 && queue.Count > 0)//(duration in seconds)
+            {
+                greenDuration -= queue.Peek().Length;
+                if(greenDuration >= 0)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            if (queue.Count == 0)
+                return 0;
+
+            if (greenDuration == 0)
+                return 0;
+
+            int index = Math.Abs(greenDuration);
+            if (index > car.Length - 1)
+            {
+                queue.Dequeue();
+                return 0;
+            }
+               
+
+            return (int)car[index];
         }
     }
 }
