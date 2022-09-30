ac8410742afb8914b3243b69ec0eb352daba17a7
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-09-30 21:59:41 +0300

    update 2. Squares in Matrix

--- C# Advanced/Multidimensional Arrays - Exercise/2. Squares in Matrix-v2/Program.cs
+++ C# Advanced/Multidimensional Arrays - Exercise/2. Squares in Matrix-v2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace _2._Squares_in_Matrix
+namespace _2._Squares_in_Matrix_v2
 {
     internal class Program
     {
@@ -10,26 +10,28 @@ namespace _2._Squares_in_Matrix
             int[] size = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             int rows = size[0];
             int cols = size[1];
-            char[,] matrix = new char[rows, cols];
+            char[][] matrix = new char[rows][];
             for (int i = 0; i < rows; i++)
             {
-                char[] colData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
-                for (int j = 0; j < cols; j++)
-                {
-                    matrix[i, j] = colData[j];
-                }
+                matrix[i] = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
             }
 
             int squares = 0;
+
             for (int i = 0; i < rows - 1; i++)
             {
                 for (int j = 0; j < cols - 1; j++)
                 {
-                    if (matrix[i, j] == matrix[i, j + 1] &&
-                        matrix[i, j] == matrix[i + 1, j] &&
-                        matrix[i, j] == matrix[i + 1, j + 1])
+                    char currElement = matrix[i][j];
+                    if (matrix[i][j] == matrix[i][j + 1])
                     {
-                        squares++;
+                        if (matrix[i][j] == matrix[i + 1][j])
+                        {
+                            if (matrix[i][j] == matrix[i + 1][j + 1])
+                            {
+                                squares++;
+                            }
+                        }
                     }
                 }
             }
