4ee92a7638a820c3297a57f7452f39ae26b0c449
TodorZarkov <t.p.zarkov@gmail.com>
2022-09-30 15:50:08 +0300

    update 12. Cups and Bottles

--- C# Advanced/Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
+++ C# Advanced/Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _12._Cups_and_Bottles
 {
@@ -6,7 +8,37 @@ namespace _12._Cups_and_Bottles
     {
         static void Main(string[] args)
         {
-            
+            Queue<int> cups = new Queue<int>();
+            Console.ReadLine().Split().Select(int.Parse).ToList().ForEach(cup => cups.Enqueue(cup));
+            Stack<int> bottles = new Stack<int>();
+            Console.ReadLine().Split().Select(int.Parse).ToList().ForEach(bottle => bottles.Push(bottle));
+
+            int waist = 0;
+            int filledWith = 0;
+
+            while (cups.Count > 0 && bottles.Count > 0)
+            {
+                int cupCapacity = cups.Peek();
+                int bottle = bottles.Pop();
+                filledWith += bottle;
+                if (filledWith >= cupCapacity)
+                {
+                    waist += (filledWith - cupCapacity);
+                    cups.Dequeue();
+                    filledWith = 0;
+                }
+            }
+
+            // here func print result
+            if (cups.Count == 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            }
+            else if (bottles.Count == 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            }
+            Console.WriteLine($"Wasted litters of water: {waist}");
         }
     }
 }
