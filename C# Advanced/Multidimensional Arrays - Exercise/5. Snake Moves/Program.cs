44715b56d1024a9b05468910125637959db5b1e6
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-10-01 14:28:09 +0300

    init 5. Snake Moves

---
+++ C# Advanced/Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Snake_Moves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dims = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = dims[0];
+            int cols = dims[1];
+
+            string snake = Console.ReadLine();//no empty spaces expected
+            char[,] matrix = new char[rows, cols];
+            int index = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+                        matrix[i, j] = snake[index];
+                        index++;
+                    }
+                }
+                else
+                {
+                    for (int j = cols - 1; j >= 0; j--)
+                    {
+                        if (index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+                        matrix[i, j] = snake[index];
+                        index++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string line = "";
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    line += $"{matrix[i, j]}";
+                }
+                Console.WriteLine(line.Trim());
+            }
+
+        }
+    }
+}
