fc8ba4ab4ae88ecbd102e9f72d926a194945e86a
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-10-03 18:27:10 +0300

    init 05. Count Symbols

--- C# Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Count_Symbols
 {
@@ -11,8 +12,16 @@ namespace _05._Count_Symbols
             Dictionary<char, int> symbols = new Dictionary<char, int>();
             foreach (char symbol in text)
             {
-
+                if (!symbols.ContainsKey(symbol))
+                {
+                    symbols.Add(symbol, 0);
+                }
+                symbols[symbol]++;
             }
+
+            symbols.OrderBy(x => x.Key).ToList().
+                ForEach(kvp => Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s"));
+            
         }
     }
 }
