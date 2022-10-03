131a6d9527db15c1b3f2e3929d02b32136d90e3b
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-03 16:06:42 +0300

    init 03. Periodic Table

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countLines = int.Parse(Console.ReadLine());
+            HashSet<string> elements = new HashSet<string>();
+            for (int i = 0; i < countLines; i++)
+            {
+                Console.ReadLine().Split().ToList().ForEach(c=>elements.Add(c));
+            }
+            List<string> sortedElements = elements.OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(' ',sortedElements));
+        }
+    }
+}
