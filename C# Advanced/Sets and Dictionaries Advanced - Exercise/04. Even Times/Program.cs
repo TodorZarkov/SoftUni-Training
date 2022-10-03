5aafaa4506834a969a5d97705a875f54e9a846d4
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-03 16:22:30 +0300

    init 04. Even Times

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            for (int i = 0; i < lines; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers.Add(number,1);
+                }
+                numbers[number] *= -1;
+            }
+            int theEvenKey = numbers.OrderByDescending(kvp => kvp.Value).ToList()[0].Key;
+            Console.WriteLine(theEvenKey);
+        }
+    }
+}
