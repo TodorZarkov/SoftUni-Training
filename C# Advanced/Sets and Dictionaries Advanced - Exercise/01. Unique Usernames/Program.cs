bf04a3bc7c5409d8544dd2c197456e4dc11da6e1
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-10-03 04:10:51 +0300

    init 01. Unique Usernames

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < count; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            names.ToList().ForEach(name => Console.WriteLine(name));
+        }
+    }
+}
