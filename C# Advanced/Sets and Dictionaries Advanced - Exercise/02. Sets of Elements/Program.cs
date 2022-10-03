99617eb897a8807f66ccda46add536c405e915d7
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-03 15:56:10 +0300

    init 02. Sets of Elements

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dims = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> firstSet = new HashSet<int>();
+            for (int i = 0; i < dims[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            HashSet<int> secondSet = new HashSet<int>();
+            for (int i = 0; i < dims[1]; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+            string result = "";
+            foreach (int key in firstSet)
+            {
+                if (secondSet.Contains(key))
+                {
+                    result += $"{key} ";
+                }
+            }
+            Console.WriteLine(result.Trim());
+        }
+    }
+}
