856811884526da619ec99f54559c4148cd9eea2a
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-03 16:37:45 +0300

    init 05. Count Symbols

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> symbols = new Dictionary<char, int>();
+            foreach (char symbol in text)
+            {
+
+            }
+        }
+    }
+}
