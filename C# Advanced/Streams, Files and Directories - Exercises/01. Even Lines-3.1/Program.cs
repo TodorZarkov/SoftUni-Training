3c40c3d64a60161899d91f8a3809b195fe7ec653
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-10-05 01:11:36 +0300

    init 01. Even Lines

---
+++ C# Advanced/Streams, Files and Directories - Exercises/01. Even Lines-3.1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder result = new StringBuilder();
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+                int lineCount = 0;
+                while (line != null)
+                {
+                    if (lineCount % 2 == 0)
+                    {
+                        string revLine = string.Join(' ', line.Split(' ').Reverse().ToArray());
+                        result.Append(revLine);
+                        result.Append('\n');
+                    }
+                    lineCount++;
+
+                    line = reader.ReadLine();
+                }
+            }
+            string[] symbols = { "-", ",", ".", "!", "?" };
+            foreach (var symbol in symbols)
+            {
+                result.Replace(symbol, "@");
+            }
+            return result.ToString();
+        }
+    }
+}
