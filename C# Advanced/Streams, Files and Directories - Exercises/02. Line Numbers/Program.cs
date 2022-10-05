469f575a05483da92809642242a09a1a2b663c57
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-05 10:46:53 +0300

    update 02. Line Numbers

--- C# Advanced/Streams, Files and Directories - Exercises/02. Line Numbers/Program.cs
+++ C# Advanced/Streams, Files and Directories - Exercises/02. Line Numbers/Program.cs
@@ -1,6 +1,33 @@
-﻿public class LineNumbers
+﻿namespace LineNumbers
 {
-    public static void ProcessLines(string inputFilePath, string outputFilePath)
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class LineNumbers
     {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            StringBuilder sb = new StringBuilder();
+            Regex lettersRx = new Regex(@"\w");
+            Regex marksRx = new Regex(@"\p{P}");
+            for (int i = 0;i<lines.Length;i++)
+            {
+                int letters = lettersRx.Matches(lines[i]).Count;
+                int marks = marksRx.Matches(lines[i]).Count;
+                sb.AppendLine($"Line {i + 1}: {lines[i]} ({letters})({marks})");
+            }
+            File.WriteAllText(outputFilePath, sb.ToString());
+        }
     }
 }
