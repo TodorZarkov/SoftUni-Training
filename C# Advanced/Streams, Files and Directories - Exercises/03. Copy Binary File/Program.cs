629f69070f38b7ddb9f27398718246e2e70a3d5c
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-05 14:48:33 +0300

    init 03. Copy Binary File

---
+++ C# Advanced/Streams, Files and Directories - Exercises/03. Copy Binary File/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream readStream = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream writeStream = new FileStream(outputFilePath, FileMode.Create))
+                {
+
+                    byte[] buffer = new byte[1];
+                    while (readStream.Read(buffer) != 0)
+                    {
+                        writeStream.Write(buffer);
+                    }
+                }
+            }
+        }
+    }
+}
