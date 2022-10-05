36348ad0f160ae026cac96915f60ba6735eead2c
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-10-06 01:58:37 +0300

    init 05. Copy Directory

---
+++ C# Advanced/Streams, Files and Directories - Exercises/05. Copy Directory/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+            DirectoryInfo dir = new DirectoryInfo(inputPath);
+
+            Directory.CreateDirectory(outputPath);
+
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                string targetFilePath = Path.Combine(outputPath, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+            
+        }
+    }
+}
