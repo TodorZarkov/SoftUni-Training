cb02599e028260caa7d4a26b8aefdaf4f69d3d7e
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-06 14:38:50 +0300

    init 02. Knights of Honor

---
+++ C# Advanced/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = line => Console.WriteLine("Sir "+line);
+            Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(print);
+        }
+    }
+}
