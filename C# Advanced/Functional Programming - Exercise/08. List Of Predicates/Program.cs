a02389c953146f83c56611a7a345adfaadfd09c9
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-07 22:17:21 +0300

    init 08. List Of Predicates

---
+++ C# Advanced/Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int[], bool> filters = (number, dividers) =>
+            {
+                bool result = true;
+                foreach (var divider in dividers)
+                {
+                    if (number % divider != 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+                return result;
+            };
+
+            int[] range = Enumerable.Range(1, int.Parse(Console.ReadLine())).ToArray();
+            int[] dividers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] filtered = range.Where(number => filters(number, dividers)).ToArray();
+            Console.WriteLine(String.Join(' ',filtered));
+            
+        }
+    }
+}
