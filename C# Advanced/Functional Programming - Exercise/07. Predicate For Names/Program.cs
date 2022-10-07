206d691dc3685506c6e6c902f98c0fee2095b833
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-07 21:19:33 +0300

    init 07. Predicate For Names

---
+++ C# Advanced/Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> filterLen = (name, len) => name.Length <= len;
+            int len = int.Parse(Console.ReadLine());
+            Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(name => filterLen(name, len)).ToList().ForEach(name => Console.WriteLine(name));
+        }
+    }
+}
