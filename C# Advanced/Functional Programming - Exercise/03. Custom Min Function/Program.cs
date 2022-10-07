4e931e40b9653d593782b4c8650aacf2ebb75559
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-10-07 11:16:28 +0300

    init 04. Find Evens or Odds

---
+++ C# Advanced/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = str => int.Parse(str);
+            int smallest = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(parser).Min();
+            Console.WriteLine(smallest);
+
+        }
+    }
+}
---
+++ C# Advanced/Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int i = 0; i < range.Length; i++)
+            {
+
+            }
+        }
+    }
+}
