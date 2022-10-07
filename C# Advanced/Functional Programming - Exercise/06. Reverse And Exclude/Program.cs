2feabd18f3b835bd2f40188c2a537bbd4b374f73
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-07 19:47:06 +0300

    init 06. Reverse And Exclude

---
+++ C# Advanced/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int divider = int.Parse(Console.ReadLine());
+
+            Func<int, int,bool> filter = (number,divider) => number%divider!=0;
+
+            int[] result = numbers.Where(n => filter(n, divider)).ToArray();
+            int[] arr = new int[result.Length];
+            int[] revResult = result.Reverse().ToArray();
+            Console.WriteLine(String.Join(' ',revResult));
+
+        }
+    }
+}
