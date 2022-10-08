a59a7b6430a43e15bb0f3a790a8f9a05cade345d
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-08 21:10:22 +0300

    init 09. Predicate Party!

--- C# Advanced/Functional Programming - Exercise/09. Predicate Party!/Program.cs
+++ C# Advanced/Functional Programming - Exercise/09. Predicate Party!/Program.cs
@@ -8,16 +8,53 @@ namespace _09._Predicate_Party_
     {
         static void Main(string[] args)
         {
-            
+
 
             List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
-            string command = Console.ReadLine();
-            while (command != "Party!")
+            string line = Console.ReadLine();
+            while (line != "Party!")
             {
+                string[] commands = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
 
+                string toDo = commands[0];
+                string condition = commands[1];
+                string arg = commands[2];
 
-                command = Console.ReadLine();
+                if (toDo == "Remove")
+                {
+                    names.RemoveAll(Filter(condition, arg));
+                }
+                else if (toDo == "Double")
+                {
+                    names.AddRange(names.FindAll(Filter(condition, arg)));
+                }
+
+                line = Console.ReadLine();
             }
+            if (names.Count != 0)
+            {
+                Console.Write(string.Join(", ", names));
+                Console.WriteLine(" are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+
+        }
+
+        static Predicate<string> Filter(string condition, string arg)
+        {
+            switch (condition)
+            {
+                case "StartsWith":
+                    return name => name.StartsWith(arg);
+                case "EndsWith":
+                    return name => name.EndsWith(arg);
+                case "Length":
+                    return name => name.Length == int.Parse(arg);
+            }
+            return null;
         }
     }
 }
