81e99aa86170a430552fcfd683cfb609d959ce3e
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-08 09:27:55 +0300

    init 07. Predicate For Names VER2

---
+++ C# Advanced/Functional Programming - Exercise/07. Predicate For Names VER2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names_VER2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Action<string[], Predicate<string>> printNames = (names, match) =>
+            {
+                foreach (string name in names)
+                {
+                    if (match(name))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            };
+
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            printNames(names, name => name.Length <= length);
+        }
+    }
+}
---
+++ C# Advanced/Functional Programming - Exercise/09. Predicate Party!/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Predicate_Party_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            string command = Console.ReadLine();
+            while (command != "Party!")
+            {
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
