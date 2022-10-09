5713db784c23337912592ac7a07c0a69fe9b0e20
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-09 08:52:18 +0300

    init 01. Define a Class Person

---
+++ C# Advanced/Defining Classes - Exercise/01. Define Class Person/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace DefiningClasses
+{
+    internal class Person
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/01. Define Class Person/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Pesho",
+                Age = 29
+            };
+            Console.WriteLine(person.Name + " " + person.Age);
+        }
+    }
+}
