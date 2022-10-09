a53e1bccd3f053ef17805fe0b82cce31cb09cdda
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-09 09:14:09 +0300

    init 02. Creating Constructors

---
+++ C# Advanced/Defining Classes - Exercise/02. Creating Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Pesho",
+                Age = 30
+            };
+
+            Person p2 = new Person(5);
+
+            Person p3 = new Person();
+            Console.WriteLine(person.Name + " " + person.Age);
+            Console.WriteLine(p2.Name + " " + p2.Age);
+            Console.WriteLine(p3.Name + " " + p3.Age);
+        }
+    }
+}
