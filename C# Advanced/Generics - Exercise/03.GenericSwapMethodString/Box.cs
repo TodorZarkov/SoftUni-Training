111db5d4475fe051da3cdc376d56c139479a1b03
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 19:41:18 +0300

    init  03. Generic Swap Method String

---
+++ C# Advanced/Generics - Exercise/03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Box<Type>
+    {
+        public Box()
+        {
+            Items = new List<Type>();
+        }
+
+        public List<Type> Items { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            Items.ForEach(item => sb.AppendLine($"{item.GetType()}: {item}"));
+            return sb.ToString();
+        }
+
+        public void Swap(int n,int m)
+        {
+            Type temp = Items[n];
+            Items[n] = Items[m];
+            Items[m] = temp;
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/03.GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string line = Console.ReadLine();
+                box.Items.Add(line);
+            }
+            int[] swapData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            box.Swap(swapData[0], swapData[1]);
+            Console.WriteLine(box);
+
+        }
+    }
+}
