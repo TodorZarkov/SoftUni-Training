6e58a69509504123a5e96eb5402e770b25392686
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 20:56:10 +0300

    init 07. Tuple

---
+++ C# Advanced/Generics - Exercise/07.Tuple/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] personData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string personName = personData[0] + " " + personData[1];
+            string personAddres = personData[2];
+            Tuple<string, string> person = new Tuple<string, string>(personName, personAddres);
+
+            string[] beerData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Tuple<string, int> manVsBeer = new Tuple<string, int>(beerData[0], int.Parse(beerData[1]));
+
+            string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Tuple<int, double> tData = new Tuple<int, double>(int.Parse(data[0]), double.Parse(data[1]));
+
+            Console.WriteLine(person);
+            Console.WriteLine(manVsBeer);
+            Console.WriteLine(tData);
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/07.Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<TI1,TI2>
+    {
+        public Tuple(TI1 item1, TI2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public TI1 Item1 { get; set; }
+        public TI2 Item2 { get; set; }
+
+        public override string ToString()
+        {
+
+            return $"{string.Join(' ',Item1)} -> {string.Join(' ', Item2)}";
+        }
+    }
+}
