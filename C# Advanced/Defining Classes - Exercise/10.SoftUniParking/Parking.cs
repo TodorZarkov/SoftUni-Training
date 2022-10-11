9386770f1410c3d1961c8dad9e5903c5daee2925
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 13:55:05 +0300

    init 10. SoftUni Parking

---
+++ C# Advanced/Defining Classes - Exercise/10.SoftUniParking/Parking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        Dictionary<string, Car> cars;
+        int capacity;
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.cars = new Dictionary<string, Car>(capacity);
+        }
+
+        public int Count { get { return this.cars.Count; } }
+
+        public string AddCar(Car car)
+        {
+            if (this.cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if (this.Count >= this.capacity)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                cars.Add(car.RegistrationNumber, car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+        }
+        public string RemoveCar(string RegistrationNumber)
+        {
+            if (!cars.ContainsKey(RegistrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            cars.Remove(RegistrationNumber);
+            return $"Successfully removed {RegistrationNumber}";
+        }
+        public Car GetCar(string RegistrationNumber)
+        {
+            return cars.GetValueOrDefault(RegistrationNumber);
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            RegistrationNumbers.ForEach(rn => cars.Remove(rn));
+        }
+    }
+}
