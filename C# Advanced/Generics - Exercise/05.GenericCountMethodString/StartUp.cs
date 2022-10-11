987fd2682d97ebf45fc3619758ab1eaa62733b96
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 20:15:39 +0300

    init 05. Generic Count Method String

--- C# Advanced/Generics - Exercise/05.GenericCountMethodString/StartUp.cs
+++ C# Advanced/Generics - Exercise/05.GenericCountMethodString/StartUp.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace _01.GenericBoxOfString
+namespace _05.GenericCountMethodString
 {
     public class StartUp
     {
         static void Main(string[] args)
         {
+            Box<string> box = new Box<string>();
             int lines = int.Parse(Console.ReadLine());
             for (int i = 0; i < lines; i++)
             {
                 string line = Console.ReadLine();
-                Box<string> box = new Box<string>(line);
-                Console.WriteLine(box);
-
+                box.Items.Add(line);
             }
+
+            string toMatch = Console.ReadLine();
+            Console.WriteLine(box.Count(box.Items,toMatch));
         }
     }
 }
