b9481ab0d5f1cece23bb1e86b82b8db088e072ee
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 21:15:18 +0300

    init 08. Threeuple

---
+++ C# Advanced/Generics - Exercise/08.Threeuple/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08.Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personData = Console.ReadLine().Split(' ', 4, StringSplitOptions.RemoveEmptyEntries);
+            string personName = personData[0] + " " + personData[1];
+            string personAddres = personData[2];
+            string personTown = personData[3];
+            Threeuple<string, string, string> person = new Threeuple<string, string, string>(personName, personAddres, personTown);
+
+            string[] beerData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Threeuple<string, int, string> manVsBeer = new Threeuple<string, int, string>(beerData[0], int.Parse(beerData[1]), beerData[2]== "drunk"?"True":"False");
+
+            string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Threeuple<string, double,string> tData = new Threeuple<string, double,string>(data[0], double.Parse(data[1]), data[2]);
+
+            Console.WriteLine(person);
+            Console.WriteLine(manVsBeer);
+            Console.WriteLine(tData);
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Threeuple
+{
+    public class Threeuple<TI1,TI2,TI3>
+    {
+        public Threeuple(TI1 item1, TI2 item2, TI3 item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+
+        public TI1 Item1 { get; set; }
+        public TI2 Item2 { get; set; }
+        public TI3 Item3 { get; set; }
+
+        public override string ToString()
+        {
+
+            return $"{Item1} -> {Item2} -> {Item3}";
+        }
+    }
+}
