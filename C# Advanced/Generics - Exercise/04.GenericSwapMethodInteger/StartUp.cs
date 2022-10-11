1a7e6d90b6b633dcc7281d3d1e510ae1b0d0f29c
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 19:45:11 +0300

    init 04. Generic Swap Method Integer

---
+++ C# Advanced/Generics - Exercise/04.GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string line = Console.ReadLine();
+                box.Items.Add(int.Parse(line));
+            }
+            int[] swapData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            box.Swap(swapData[0], swapData[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
