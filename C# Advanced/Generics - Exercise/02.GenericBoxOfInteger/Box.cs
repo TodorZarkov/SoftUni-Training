d2fad1c5e740f5d5419e756384a5eff8b7490455
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 19:14:43 +0300

    init 02. Generic Box of Integer

---
+++ C# Advanced/Generics - Exercise/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class Box<Type>
+    {
+        public Box(Type variale)
+        {
+            Variale = variale;
+        }
+
+        public Type Variale { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Variale.GetType()}: {Variale}";
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                int line = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(line);
+                Console.WriteLine(box);
+
+            }
+        }
+    }
+}
