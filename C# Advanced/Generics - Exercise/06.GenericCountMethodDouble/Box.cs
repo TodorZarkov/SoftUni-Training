70edcb854e833338d2e73f9c1840a48415ec88a1
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 20:20:24 +0300

    init 06. Generic Count Method Double

---
+++ C# Advanced/Generics - Exercise/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<Type> where Type:IComparable
+    {
+        public Box()
+        {
+            Items = new List<Type>();
+        }
+
+        public List<Type> Items { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            Items.ForEach(item => sb.AppendLine($"{item.GetType()}: {item}"));
+            return sb.ToString();
+        }
+
+        public void Swap(int n,int m)
+        {
+            Type temp = Items[n];
+            Items[n] = Items[m];
+            Items[m] = temp;
+        }
+
+        public int Count(List<Type> items, Type token)
+        {
+            int result = 0;
+            foreach (var item in items)
+            {
+                if (item.CompareTo(token)==1)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/06.GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+
+            int numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string line = Console.ReadLine();
+                box.Items.Add(double.Parse(line));
+            }
+
+            string toMatch = Console.ReadLine();
+            Console.WriteLine(box.Count(box.Items, double.Parse(toMatch)));
+        }
+    }
+}
