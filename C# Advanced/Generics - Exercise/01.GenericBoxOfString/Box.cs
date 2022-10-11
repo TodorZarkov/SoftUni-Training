f1ae0cd8dcd5c5d451f9839affeee70918612761
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-11 19:09:23 +0300

    init 01. Generic Box of String

---
+++ C# Advanced/Generics - Exercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxOfString
+{
+    public class Box<Type>
+    {
+        public Box(Type variale)
+        {
+            Variale = variale;
+        }
+
+        public Type Variale { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Variale.GetType()}: {Variale}";
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string line = Console.ReadLine();
+                Box<string> box = new Box<string>(line);
+                Console.WriteLine(box);
+                
+            }
+        }
+    }
+}
