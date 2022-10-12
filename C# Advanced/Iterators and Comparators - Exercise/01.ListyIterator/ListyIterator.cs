793f15fbb74166126cce1ad956177a4c024b0377
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-12 22:32:06 +0300

    init 01. ListyIterator

---
+++ C# Advanced/Iterators and Comparators - Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> elements;
+        private int index;
+        private int count;
+        public ListyIterator()
+        {
+            count = 0;
+            index = 0;
+            elements = new List<T>();
+
+        }
+        public ListyIterator(params T[] inputElements)
+        {
+            count = inputElements.Length;
+            index = 0;
+            elements = new List<T>(inputElements);
+        }
+
+        public bool Move()
+        {
+            if (index + 1 >= count)
+            {
+                return false;
+            }
+            index++;
+            return true;
+        }
+        public bool HasNext()
+        {
+            if (index + 1 >= count)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void Print()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(elements[index]);
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/01.ListyIterator/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace _01.ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> list;
+
+            string[] create = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (create.Length == 1)
+            {
+                list = new ListyIterator<string>();
+            }
+            else
+            {
+                string[] parameters = new string[create.Length - 1];
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    parameters[i] = create[i + 1];
+                }
+                list = new ListyIterator<string>(parameters);
+            }
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (InvalidOperationException exeption)
+                        {
+
+                            Console.WriteLine(exeption.Message);
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
