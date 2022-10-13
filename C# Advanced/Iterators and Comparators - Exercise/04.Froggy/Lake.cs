bd0ebe4d548bf6835723c3f011de78394cc8efea
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-10-13 06:18:18 +0300

    init 04. Froggy

---
+++ C# Advanced/Iterators and Comparators - Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    internal class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+        public Lake(params int[] stones)
+        {
+            this.stones = new List<int>(stones);
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i+=2)
+            {
+                yield return stones[i];
+            }
+            int nextStart = stones.Count - 1;
+            if (nextStart % 2 ==0)
+            {
+                nextStart--;
+            }
+            for (int i = nextStart; i > 0; i-=2)
+            {
+                yield return stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/04.Froggy/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Lake lake = new Lake(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var stone in lake)
+            {
+                sb.Append(stone);
+                sb.Append(", ");
+            }
+            sb.Remove(sb.Length - 2, 1);
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
