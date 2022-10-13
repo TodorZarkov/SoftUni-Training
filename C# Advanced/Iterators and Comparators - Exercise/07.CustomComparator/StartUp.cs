4749d8a945d62180c057fbf929b258987249d5d1
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-13 21:28:32 +0300

    init 07. Custom Comparator

---
+++ C# Advanced/Iterators and Comparators - Exercise/07.CustomComparator/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace _07.CustomComparator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ComparatorEvenOdds compar = new ComparatorEvenOdds();
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Array.Sort<int>(arr, compar);
+            Console.WriteLine(String.Join(' ',arr));
+        }
+    }
+
+    class ComparatorEvenOdds : IComparer<int>
+    {
+        public int Compare([AllowNull] int x, [AllowNull] int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+            if (x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+            return x - y;
+        }
+    }
+}
