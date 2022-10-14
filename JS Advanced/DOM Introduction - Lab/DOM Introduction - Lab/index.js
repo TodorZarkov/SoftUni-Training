ccfada7b24055ccc511238ed588db0de548b76f0
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-14 21:16:36 +0300

    init dom lab

---
+++ JS Advanced/DOM Introduction - Lab/DOM Introduction - Lab/index.js
@@ -0,0 +1,28 @@
+
+isSet = false;
+
+function changeText() {
+    let header = document.getElementsByTagName('h1');
+    let input = document.getElementById('my-data');
+    let value = input.value;
+
+    header[0].innerHTML = `<p>${value}</p>`;
+    header[0].style.color = 'red';
+
+    if (isSet) return;
+
+    isSet = true;
+    let fontSize = 30;
+    setInterval(() => {
+        fontSize++;
+        header[0].style.fontSize = `${fontSize}px`;
+        if (fontSize%2===0) {
+
+            header[0].style.color = 'blue';
+        } else {
+            header[0].style.color = 'red';
+        }
+        console.log(fontSize);
+    }, 100)
+
+}
