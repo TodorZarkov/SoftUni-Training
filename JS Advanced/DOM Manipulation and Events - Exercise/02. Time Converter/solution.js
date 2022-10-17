c56725ce30b4c1f305aa029a5d5d3d1a3a8a7cc8
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-17 14:26:42 +0300

    init 02. Time Converter

--- JS Advanced/DOM Manipulation and Events - Exercise/02. Time Converter/solution.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/02. Time Converter/solution.js
@@ -1,4 +1,33 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let main = document.querySelector('main');
+    main.addEventListener('click', (event)=>{
+        let daysField = document.getElementById('days');
+        let hoursField = document.getElementById('hours');
+        let minutesField = document.getElementById('minutes');
+        let secondsField = document.getElementById('seconds');
+
+        let clicked = event.target;
+
+        let seconds = 0;
+        if (clicked.id === 'daysBtn') {
+            let days = Number(daysField.value);
+            seconds = 86400 * days;
+        } else if (clicked.id === 'hoursBtn') {
+            let hours = Number(hoursField.value);
+            seconds = 3600 * hours;
+        } else if (clicked.id === 'minutesBtn') {
+            let minutes = Number(minutesField.value);
+            seconds = 60 * minutes
+        } else if (clicked.id === 'secondsBtn') {
+            seconds = Number(secondsField.value);
+        }
+
+        daysField.value = seconds / 86400;
+        hoursField.value = seconds / 3600;
+        minutesField.value = seconds / 60;
+        secondsField.value = seconds;
+    });
 }
+
+
