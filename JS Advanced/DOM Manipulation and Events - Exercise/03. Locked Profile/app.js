07e8df73b24adcc2f401c75ea255b2585f8effe9
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-17 22:19:26 +0300

    init 03. Locked Profile

---
+++ JS Advanced/DOM Manipulation and Events - Exercise/03. Locked Profile/app.js
@@ -0,0 +1,13 @@
+function lockedProfile() {
+    let main = document.getElementById('main');
+    main.addEventListener('click', onClick);
+
+    function onClick(event){
+        let clickElement = event.target;
+        if(clickElement.tagName !== 'BUTTON'){
+            return;
+        }
+        
+        let radio = 
+    }
+}
