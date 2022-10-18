b5e22389699d97ca3e4cb3b80920e8f1e0ec5a6a
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-18 05:37:36 +0300

    update 03. Locked Profile

--- JS Advanced/DOM Manipulation and Events - Exercise/03. Locked Profile/app.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/03. Locked Profile/app.js
@@ -2,12 +2,24 @@ function lockedProfile() {
     let main = document.getElementById('main');
     main.addEventListener('click', onClick);
 
-    function onClick(event){
+    function onClick(event) {
         let clickElement = event.target;
-        if(clickElement.tagName !== 'BUTTON'){
+        if (clickElement.tagName !== 'BUTTON') {
             return;
         }
-        
-        let radio = 
+
+        console.log(clickElement.parentElement.querySelector('input[type="radio"][value="lock"]'));
+        //let lockBtn = clickElement.parentElement.querySelector('input[type="radio"][value="lock"]');
+        let unlockBtn = clickElement.parentElement.querySelector('input[type="radio"][value="unlock"]');
+        if (unlockBtn.checked) {
+            if (clickElement.textContent === 'Show more') {
+                clickElement.textContent = 'Hide it';
+                clickElement.parentElement.querySelector('div[id]').style.display = 'block';
+            } else if (clickElement.textContent === 'Hide it') {
+                clickElement.textContent = 'Show more';
+                clickElement.parentElement.querySelector('div[id]').style.display = 'none';
+            }
+        }
+
     }
 }
