f99ce6a28ac38f62507ddac555978624a5404d8f
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-27 16:50:06 +0300

    init 05. Restaurant

---
+++ CSharpOOP/Inheritance - Exercise/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public Cake(string name) : base(name, 5m, 250, 1000)
+        {
+
+        }
+        //public Cake(string name, decimal price, double grams, double calories) : base(name, price, grams, calories)
+        //{
+
+        //}
+    }
+}
---
+++ CSharpOOP/Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, double caffeine) : base(name, 3.5m, 50)
+        {
+            Caffeine = caffeine;
+        }
+        public double Caffeine { get; set; }
+    }
+}
