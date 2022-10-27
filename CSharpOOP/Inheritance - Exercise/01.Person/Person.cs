0b1d8b5bb8ee05f93a13baa0165de18824d07d87
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-27 12:52:05 +0300

    init 01. Person

---
+++ CSharpOOP/Inheritance - Exercise/01.Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get { return this.name; } set { this.name = value; } }
+        public virtual int Age 
+        {
+            get { return age; }
+            set 
+            {
+                if(value < 0)
+                {
+                    // throw new ArgumentOutOfRangeException("The age cannot be negative!");
+                    age = 0;
+                }
+                age = value;
+            } 
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {Name}, Age: {Age}");
+
+            return sb.ToString();
+        }
+    }
+}
