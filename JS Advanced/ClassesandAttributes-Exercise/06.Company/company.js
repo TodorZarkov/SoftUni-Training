5047e9ef6195829dc7316a860ad96c60c36b85bb
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-31 15:56:34 +0200

    init 01. Bus Stop

---
+++ JS Advanced/ClassesandAttributes-Exercise/06.Company/company.js
@@ -0,0 +1,5 @@
+class Company {
+    constructor() {
+        departments = {};
+    }
+}
--- JSApplications/AsynchronousProgramming-Exercise/01.Bus-Stop/app.js
+++ JSApplications/AsynchronousProgramming-Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,41 @@
 function getInfo() {
-    console.log("TODO...");
+    let stopId = document.getElementById('stopId').value;
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+        .then(onResponse)
+        .then(onData)
+        .catch(onError);
+
+
+    function onResponse(response) {
+        if (response.ok === false || response.status === 204 || stopId === '') {
+            throw "Error";
+        }
+        return response.json();
+    }
+
+    function onData(data) {
+        let stopNameDiv = document.getElementById("stopName");
+        stopNameDiv.textContent = data.name;
+        let bussesUl = document.getElementById("buses");
+        bussesUl.innerHTML = '';
+
+        for (let key in data.buses) {
+            let li = document.createElement('li');
+            let innerText = `Bus ${key} arrives in ${data.buses[key]} minutes`;
+            li.textContent = innerText;
+            bussesUl.appendChild(li);
+        }
+        console.log(data);
+        console.log(data.buses);
+
+    }
+
+    function onError(e) {
+        let stopNameDiv = document.getElementById("stopName");
+        stopNameDiv.textContent = "Error";
+        let bussesUl = document.getElementById("buses");
+        bussesUl.innerHTML = '';
+    }
+
 }
