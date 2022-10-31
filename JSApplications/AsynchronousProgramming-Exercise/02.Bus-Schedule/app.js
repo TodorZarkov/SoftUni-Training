8524001ad3df404a7522822080e9239efbb4e2fb
TodorZarkov <t.p.zarkov@gmail.com>
2022-10-31 19:51:41 +0200

    init 02. Bus Schedule

--- JSApplications/AsynchronousProgramming-Exercise/02.Bus-Schedule/app.js
+++ JSApplications/AsynchronousProgramming-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,38 @@
 function solve() {
+    let stopId = 'depot';
+    let currentStopName = '';
+    let infoBox = document.getElementById('info').getElementsByTagName('span')[0];
+    let departBtn = document.getElementById("depart");
+    let arriveBtn = document.getElementById("arrive");
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${stopId}`)
+        .then(onResponse)
+        .then(onData)
+        .catch(onError);
+    }
+    function onResponse(response){
+        if(response.ok === false || response.status !== 200){
+            throw "Error";
+        }
+        return response.json();
+    }
+    function onData(data){
+        currentStopName = data.name;
+        infoBox.textContent = `Next stop ${data.name}`;
+        stopId = data.next;
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+
+    }
+    function onError(e){
+        infoBox.textContent = 'Error';
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${currentStopName}`
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
