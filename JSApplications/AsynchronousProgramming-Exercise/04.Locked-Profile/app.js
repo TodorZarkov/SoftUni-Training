f33ec68aa2164d5756a5a67338aecfce09aca495
TodorZarkov <t.p.zarkov@gmail.com>
2022-11-01 18:10:34 +0200

    init 04.Locked-Profile

--- JSApplications/AsynchronousProgramming-Exercise/04.Locked-Profile/app.js
+++ JSApplications/AsynchronousProgramming-Exercise/04.Locked-Profile/app.js
@@ -1,3 +1,21 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    const urlProfiles = "http://localhost:3030/jsonstore/advanced/profiles";
+    let profiles = {};
+    try {
+        let response = await fetch(urlProfiles);
+        if (response.ok === false || response.status !== 200) throw "Bad URI";
+        profiles = await response.json();
+    } catch (error) {
+        onError(error);
+    }
+
+    const containerProfiles = document.getElementById("main");
+    for(let profile in profiles){
+        
+    }
 }
+
+function onError(e) {
+    console.log(e);
+}
+
