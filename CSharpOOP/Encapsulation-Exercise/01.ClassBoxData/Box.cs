97b74417c67bdc18fddaa681e7065276e3a65469
TodorZarkov <t.p.zarkov@gmail.com>
2022-11-02 13:30:37 +0200

    init 01. Class Box Data

---
+++ CSharpOOP/Encapsulation-Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        double length;
+        double height;
+        double width;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Height = height;
+            Width = width;
+        }
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+
+        public double SurfaceArea()
+        {
+            return 2 * (Length * Width + Length * Height + Width * Height);
+        }
+        public double LateralSurfaceArea()
+        {
+            return 2 * Height * (Width + Length);
+        }
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+
+    }
+}
---
+++ CSharpOOP/Encapsulation-Exercise/01.ClassBoxData/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(lenght, width, heigth);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+        }
+    }
+}
