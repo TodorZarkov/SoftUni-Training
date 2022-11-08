9fbd5fdc054070219824c44a924694920ae27930
TodorZarkov <t.p.zarkov@gmail.com>
2022-11-08 20:55:42 +0200

    #

--- JSApplications/DataAndAuthentication-Exercise/05.Fisher-Game/src/login.js
+++ JSApplications/DataAndAuthentication-Exercise/05.Fisher-Game/src/login.js
@@ -1,1 +1,47 @@
-console.log('TODO:// Implement Login functionality');
+const loginUrl = "http://localhost:3030/users/login";
+const afterSuccessUrl = "./index.html"
+
+loadListeners();
+
+//control
+function loadListeners() {
+    document.getElementById("login-form").addEventListener('submit', onLogin);
+}
+
+async function onLogin(event) {
+    event.preventDefault();
+    let formEl = event.target;
+    let form = new FormData(formEl);
+    let email = form.get("email");
+    let password = form.get("password");
+    let user;
+    try {
+        user = await login(email, password);
+    } catch (error) {
+        //all errors
+        window.alert(error);
+        return;
+    }
+    
+    sessionStorage.setItem("accessToken",user.accessToken);
+    window.location.replace(afterSuccessUrl);
+}
+
+//view
+
+
+//model
+async function login(email, password) {
+    let regObj = { email, password };
+    let user = null;
+
+    let response = await fetch(loginUrl, {
+        method: "post",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(regObj)
+    });
+    user = await response.json();
+    if (response.ok === false || response.status !== 200) throw user.message;
+
+    return user;
+}
--- JSApplications/DataAndAuthentication-Exercise/05.Fisher-Game/src/register.js
+++ JSApplications/DataAndAuthentication-Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,53 @@
-console.log('TODO:// Implement Register functionality');
+const regUrl = "http://localhost:3030/users/register";
+const afterSuccessUrl = "./index.html"
+
+loadListeners();
+
+//control
+function loadListeners() {
+    document.getElementById("register-form").addEventListener('submit', onRegister);
+}
+
+async function onRegister(event) {
+    event.preventDefault();
+    let formEl = event.target;
+    let form = new FormData(formEl);
+    let email = form.get("email");
+    let password = form.get("password");
+    let rePass = form.get("rePass");
+    if (password !== rePass) {
+        window.alert("Repeat doesn't match Password");
+        return;
+    }
+    let newUser;
+    try {
+        newUser = await register(email, password);
+    } catch (error) {
+        //all errors
+        window.alert(error);
+        return;
+    }
+    
+    sessionStorage.setItem("accessToken",newUser.accessToken);
+    window.location.replace(afterSuccessUrl);
+}
+
+//view
+
+
+//model
+
+async function register(email, password) {
+    let regObj = { email, password };
+    let newUser = null;
+
+    let response = await fetch(regUrl, {
+        method: "post",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(regObj)
+    });
+    newUser = await response.json();
+    if (response.ok === false || response.status !== 200) throw newUser.message;
+
+    return newUser;
+}
