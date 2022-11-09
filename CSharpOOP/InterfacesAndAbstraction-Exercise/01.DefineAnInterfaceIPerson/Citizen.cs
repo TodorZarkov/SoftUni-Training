5c5f24ff503debd2c183b0e22d583a789d616ddc
TodorZarkov <t.p.zarkov@gmail.com>
2022-11-09 13:29:22 +0200

    init 01. Define an Interface IPerson & 02. Multiple Implementation softuni problems

---
+++ CSharpOOP/InterfacesAndAbstraction-Exercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+namespace PersonInfo
+{
+    using System;
+    public class Citizen : IPerson,IIdentifiable,IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthdate;
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("The name must not be empty");
+                }
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be positive.");
+                }
+                age = value;
+            }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("The id cannot be empty.");
+                }
+                id = value;
+            }
+        }
+
+        public string Birthdate
+        {
+            get { return birthdate; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Birthdate cannot be empty.");
+                }
+                birthdate = value;
+            }
+        }
+    }
+}
