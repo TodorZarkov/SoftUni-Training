211b17b5cf39b41f6135f16525ac68528386bea3
TodorZarkov <t.p.zarkov@gmail.com>
2022-11-09 23:19:25 +0200

    init 07. Military Elite

---
+++ CSharpOOP/InterfacesAndAbstraction-Exercise/07.MilitaryElite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.MilitaryElite
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ CSharpOOP/InterfacesAndAbstraction-Exercise/07.MilitaryElite/Soldier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite
+{
+    internal class Soldier
+    {
+        string id;
+        string firstName;
+        string lastName;
+    }
+}
