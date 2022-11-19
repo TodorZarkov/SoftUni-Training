4008f3bd94ab17bd0d9bab1a67a872d6bb028a51
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2022-11-19 18:11:34 +0200

    update 01. Command Pattern

--- CSharpOOP/ReflectionAndAttributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
+++ CSharpOOP/ReflectionAndAttributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -2,6 +2,8 @@
 namespace CommandPattern.Core
 {
     using System;
+    using System.Linq;
+    using System.Reflection;
 
     using Contracts;
     using Exceptions;
@@ -13,14 +15,24 @@ namespace CommandPattern.Core
             string[] tokens = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
             string command = tokens[0];
-            string[] arguments = new string[tokens.Length - 1];
-            tokens.CopyTo(arguments, 1);
-            
-            Type type = Type.GetType(command + "Command");
+            string[] arguments = tokens.Skip(1).ToArray();
+
+            var assembly = Assembly.GetEntryAssembly();
+
+
+            Type type = assembly.GetTypes().FirstOrDefault(t => t.Name == (command + "Command"));
             if (type == null)
             {
                 throw new NoSuchCommandException();
             }
+
+            var execute = type.GetMethods().FirstOrDefault
+                (m => m.Name == "Execute");
+            if (execute == null)
+            {
+                throw new MethodNotImplementedException();
+            }
+
 
             var objCommand = (ICommand)Activator.CreateInstance(type);
 
---
+++ CSharpOOP/ReflectionAndAttributes-Exercise/CommandPattern/Core/Exceptions/MethodNotImplementedException.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandPattern.Core.Exceptions
+{
+    using System;
+
+    internal class MethodNotImplementedException:Exception
+    {
+        private const string defaultMessage = "The invoked command doesn't implement the method Execute.";
+
+        public MethodNotImplementedException() : base(defaultMessage)
+        {
+        }
+
+        public MethodNotImplementedException(string message) : base(message)
+        {
+        }
+    }
+}
