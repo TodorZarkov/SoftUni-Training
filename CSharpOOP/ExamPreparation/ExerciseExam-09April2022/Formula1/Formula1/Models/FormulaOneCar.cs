1091f6b8922e3a9fabe240f0f3df28b10878bef2
TodorZarkov <t.p.zarkov@gmail.com>
2022-12-09 16:15:17 +0200

    init exam preparation 1

---
+++ CSharpOOP/ExamPreparation/ExerciseExam-09April2022/Formula1/Formula1/Models/FormulaOneCar.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Formula1.Models
+{
+    using Contracts;
+    using Formula1.Utilities;
+    using System;
+    using System.Threading;
+
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        const int minNameLength = 3;
+        const int minHorsepower = 900;
+        const int maxHorsepower = 1015;
+        const double minDisplacement = 1.6;
+        const double maxDisplacement = 2;
+
+
+        string model;
+        int horsepower;
+        double engineDisplacement;
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < minNameLength)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+                model = value;
+            }
+        }
+
+        public int Horsepower
+        {
+            get
+            {
+                return horsepower;
+            }
+            private set
+            {
+                if (value < minHorsepower || value > maxHorsepower)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1HorsePower, value));
+                }
+                horsepower = value;
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get
+            {
+                return engineDisplacement;
+            }
+            set
+            {
+                if (value < minDisplacement || value > maxDisplacement)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1EngineDisplacement, value));
+                }
+                engineDisplacement = value;
+            }
+        }
+
+        public double RaceScoreCalculator(int laps)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
