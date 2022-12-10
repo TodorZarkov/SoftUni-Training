53ac0cc04e3569cd297efb10d15ebc0c5b23b0a0
TodorZarkov <t.p.zarkov@gmail.com>
2022-12-10 02:25:52 +0200

    update exam preparation 1

---
+++ CSharpOOP/ExamPreparation/ExerciseExam-09April2022/Formula1/Formula1/Core/Controller.cs
@@ -0,0 +1,123 @@
+﻿namespace Formula1.Core
+{
+    using Formula1.Core.Contracts;
+    using Formula1.Models;
+    using Formula1.Models.Concretes;
+    using Formula1.Models.Contracts;
+    using Formula1.Repositories;
+    using Formula1.Utilities;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    internal class Controller : IController
+    {
+        PilotRepository pilotRepository;
+        RaceRepository raceRepository;
+        FormulaOneCarRepository carRepository;
+
+        public Controller()
+        {
+            pilotRepository = new PilotRepository();
+            raceRepository = new RaceRepository();
+            carRepository = new FormulaOneCarRepository();
+        }
+        public string AddCarToPilot(string pilotName, string carModel)
+        {
+            IPilot pilot = pilotRepository.FindByName(pilotName);
+            if (pilot == null || pilot.CanRace)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotDoesNotExistOrHasCarErrorMessage, pilotName));
+            }
+            IFormulaOneCar car = carRepository.FindByName(carModel);
+            if (car == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.CarDoesNotExistErrorMessage, carModel));
+            }
+
+            pilot.AddCar(car);
+            carRepository.Remove(car);
+            return string.Format(OutputMessages.SuccessfullyPilotToCar, pilotName, car.GetType().Name, carModel);
+        }
+
+        public string AddPilotToRace(string raceName, string pilotFullName)
+        {
+            IRace race = raceRepository.FindByName(raceName);
+            IPilot pilot = pilotRepository.FindByName(pilotFullName);
+            if (race == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.RaceDoesNotExistErrorMessage, raceName));
+            }
+
+            if(pilot == null || !pilot.CanRace || race.Pilots.Contains(pilot))
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotDoesNotExistErrorMessage, pilotFullName));
+            }
+            race.AddPilot(pilot);
+            return string.Format(OutputMessages.SuccessfullyAddPilotToRace, pilotFullName, raceName);
+        }
+
+        public string CreateCar(string type, string model, int horsepower, double engineDisplacement)
+        {
+            if(carRepository.FindByName(model) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.CarExistErrorMessage, model));
+            }
+
+            Assembly assembly = Assembly.GetEntryAssembly();
+            Type carTypeToBe = assembly.GetTypes().FirstOrDefault(t => t.Name == type);
+
+            if(carTypeToBe == null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidTypeCar, type));
+            }
+
+            IFormulaOneCar car = (IFormulaOneCar)Activator.CreateInstance(carTypeToBe, model, horsepower, engineDisplacement);
+
+            carRepository.Add(car);
+
+            return String.Format(OutputMessages.SuccessfullyCreateCar, type, model);
+
+        }
+
+        public string CreatePilot(string fullName)
+        {
+            if (pilotRepository.FindByName(fullName) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotExistErrorMessage, fullName));
+            }
+            IPilot pilot = new Pilot(fullName);
+            pilotRepository.Add(pilot);
+
+            return String.Format(OutputMessages.SuccessfullyCreatePilot, fullName);
+        }
+
+        public string CreateRace(string raceName, int numberOfLaps)
+        {
+            if (raceRepository.FindByName(raceName) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.RaceExistErrorMessage, raceName));
+            }
+
+            IRace race = new Race(raceName, numberOfLaps);
+            raceRepository.Add(race);
+
+            return string.Format(OutputMessages.SuccessfullyCreateRace, raceName);
+        }
+
+        public string PilotReport()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string RaceReport()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string StartRace(string raceName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
