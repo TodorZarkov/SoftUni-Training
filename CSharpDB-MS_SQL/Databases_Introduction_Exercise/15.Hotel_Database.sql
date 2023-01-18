2c20697f2bbf9531cc9eb7fd9cd65ddaee95c758
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-18 23:09:51 +0200

    update ms-sql intro p15

--- CSharpDB-MS_SQL/Databases_Introduction_Exercise/15.Hotel_Database.sql
+++ CSharpDB-MS_SQL/Databases_Introduction_Exercise/15.Hotel_Database.sql
@@ -13,6 +13,13 @@ CREATE TABLE Employees
     Notes NVARCHAR(MAX) 
 )
 
+INSERT INTO Employees
+VALUES
+    (1, 'EMPL1FirstName', 'EMPL1LastName', 'Title1', NULL),
+    (2, 'EMPL2FirstName', 'EMPL2LastName', 'Title2', NULL),
+    (3, 'EMPL3FirstName', 'EMPL3LastName', 'Title3', NULL)
+
+
 CREATE TABLE Customers 
 (
     AccountNumber NVARCHAR(100) PRIMARY KEY, 
@@ -24,23 +31,52 @@ CREATE TABLE Customers
     Notes NVARCHAR(MAX)
 )
 
+INSERT INTO Customers 
+VALUES
+    ('AccountNumber1', 'FirstName1', 'LastName1', 'PhoneNumber1', NULL, NULL, NULL),
+    ('AccountNumber2', 'FirstName2', 'LastName2', 'PhoneNumber2', NULL, NULL, NULL),
+    ('AccountNumber3', 'FirstName3', 'LastName3', 'PhoneNumber3', NULL, NULL, NULL)
+
+--------------------------------------------
 CREATE TABLE RoomStatus 
 (
-    RoomStatus NVARCHAR(60) NOT NULL, 
+    RoomStatus NVARCHAR(60) PRIMARY KEY, 
     Notes NVARCHAR(MAX)
 )
+
+INSERT INTO RoomStatus
+VALUES 
+    ('RoomStatus1', NULL),
+    ('RoomStatus2', NULL),
+    ('RoomStatus3', NULL)
+
 
 CREATE TABLE RoomTypes
 (
-    RoomType NVARCHAR(60) NOT NULL, 
+    RoomType NVARCHAR(60) PRIMARY KEY, 
     Notes NVARCHAR(MAX)
 )
 
+INSERT INTO RoomTypes
+VALUES 
+    ('RoomType1', NULL),
+    ('RoomType2', NULL),
+    ('RoomType3', NULL)
+
+
 CREATE TABLE BedTypes 
 (
-    BedType NVARCHAR(60) NOT NULL, 
+    BedType NVARCHAR(60) PRIMARY KEY, 
     Notes NVARCHAR(MAX)
 )
+
+INSERT INTO BedTypes
+VALUES 
+    ('BedType1', NULL),
+    ('BedType2', NULL),
+    ('BedType3', NULL)
+----------------------------------------------------
+
 
 CREATE TABLE Rooms 
 (
@@ -55,6 +91,13 @@ CREATE TABLE Rooms
     Notes NVARCHAR(MAX)
 )
 
+INSERT INTO Rooms
+VALUES 
+    ('01', 'RoomType1', 'BedType1', 10.5, 'RoomStatus1', NULL),
+    ('02', 'RoomType2', 'BedType2', 20.5, 'RoomStatus2', NULL),
+    ('03', 'RoomType3', 'BedType3', 30.5, 'RoomStatus3', NULL)
+
+
 CREATE TABLE Payments 
 (
     Id BIGINT PRIMARY KEY IDENTITY, 
@@ -68,19 +111,35 @@ CREATE TABLE Payments
     TotalDays SMALLINT NOT NULL, 
     AmountCharged SMALLMONEY NOT NULL, 
     TaxRate REAL NOT NULL, 
-    TaxAmount SMALLMONEY NOT NULL, 
+    TaxAmount SMALLMONEY NOT NULL,
     PaymentTotal SMALLMONEY NOT NULL, 
     Notes NVARCHAR(MAX)
 )
 
+INSERT INTO Payments
+VALUES
+    (1, '2022-12-21', 'AccountNumber1', '2022-12-10', '2022-12-21', 11, 500.50, 0.2, 100.1, 600.60, NULL),
+    (2, '2022-12-21', 'AccountNumber2', '2022-12-10', '2022-12-21', 11, 500.50, 0.2, 100.1, 600.60, NULL),
+    (3, '2022-12-21', 'AccountNumber3', '2022-12-10', '2022-12-21', 11, 500.50, 0.2, 100.1, 600.60, NULL)
+
+
 CREATE TABLE Occupancies 
 (
-    Id INT PRIMARY, 
-    EmployeeId, 
-    DateOccupied, 
-    AccountNumber, 
-    RoomNumber, 
-    RateApplied, 
-    PhoneCharge, 
+    Id INT PRIMARY KEY, 
+    EmployeeId INT,
+        FOREIGN KEY(EmployeeId) REFERENCES  Employees(Id),
+    DateOccupied DATE NOT NULL, 
+    AccountNumber NVARCHAR(100), 
+        FOREIGN KEY(AccountNumber) REFERENCES Customers(AccountNumber),
+    RoomNumber VARCHAR(4), 
+        FOREIGN KEY(RoomNumber) REFERENCES Rooms(RoomNumber),
+    RateApplied SMALLMONEY NOT NULL, 
+    PhoneCharge SMALLMONEY, 
     Notes NVARCHAR(MAX)
 )
+
+INSERT INTO Occupancies
+VALUES  
+    (1, 1, '2022-12-21', 'AccountNumber1', '01', 50.1, NULL, NULL),
+    (2, 2, '2022-12-21', 'AccountNumber2', '02', 50.1, NULL, NULL),
+    (3, 3, '2022-12-21', 'AccountNumber3', '03', 50.1, NULL, NULL)
