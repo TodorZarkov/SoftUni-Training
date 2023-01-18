b38b342baadf949e4a4a6399482f71ceb7753b26
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-19 01:13:05 +0200

    update ms-sql intro p18

--- CSharpDB-MS_SQL/Databases_Introduction_Exercise/18.Basic_Insert.sql
+++ CSharpDB-MS_SQL/Databases_Introduction_Exercise/18.Basic_Insert.sql
@@ -20,6 +20,6 @@ INSERT INTO Employees(FirstName, MiddleName, LastName, JobTitle, DepartmentId, H
 VALUES
     ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-2-1', 3500.00, NULL),
     ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-3-2', 4000.00, NULL),
-    ('Maria', 'Petrova', ' Ivanova', 'Intern', 5, '2016-8-28', 525.25, NULL),
-    ('Georgi', ' Teziev', ' Ivanov', 'CEO', 2, '2007-12-9', 3000.00, NULL),
+    ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-8-28', 525.25, NULL),
+    ('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '2007-12-9', 3000.00, NULL),
     ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-8-28', 599.88, NULL)
