8f11696a8fede8f26fcd6cde1f429d3f0dca825f
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-19 00:34:22 +0200

    init ms-sql intro p17

---
+++ CSharpDB-MS_SQL/Databases_Introduction_Exercise/17.Backup_Database.sql
@@ -0,0 +1,15 @@
+BACKUP DATABASE SoftUni
+TO DISK = '/var/opt/mssql/data/softuni-backup.bak'
+
+USE master
+GO
+
+ALTER DATABASE SoftUni SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+GO
+
+DROP DATABASE SoftUni
+GO
+
+RESTORE DATABASE SoftUni
+    FROM DISK = '/var/opt/mssql/data/softuni-backup.bak'
+
