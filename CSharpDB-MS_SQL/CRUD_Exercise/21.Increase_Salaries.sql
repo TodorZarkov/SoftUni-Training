92878db9d7018b4a90a4ec44c9e4ea59f901bd76
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-20 15:19:23 +0200

    init CRUD problem 21. Increase Salaries

---
+++ CSharpDB-MS_SQL/CRUD_Exercise/21.Increase_Salaries.sql
@@ -0,0 +1,20 @@
+USE SoftUni
+GO
+
+UPDATE Employees
+SET Salary *= 1.12
+WHERE DepartmentID IN
+(
+    SELECT DepartmentID FROM Departments
+    WHERE [Name] IN('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+)
+
+SELECT Salary FROM Employees
+
+UPDATE Employees
+SET Salary /= 1.12
+WHERE DepartmentID IN
+(
+    SELECT DepartmentID FROM Departments
+    WHERE [Name] IN('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+)
