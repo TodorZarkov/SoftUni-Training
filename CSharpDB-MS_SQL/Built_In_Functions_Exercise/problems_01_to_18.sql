040298c0c9210554534dbdfaf9c3346c67b00203
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-31 15:29:51 +0200

    init Built-in Functions -10. RankEmployeesBySalary

--- CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
@@ -52,3 +52,10 @@ GO
 SELECT FirstName, LastName
 FROM Employees
 WHERE LEN([LastName]) = 5
+
+--problem 10. Rank Employees by Salary
+SELECT EmployeeID, FirstName, LastName, Salary
+    ,DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
