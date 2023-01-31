d2b6ef389c41a27c5fd2888fff1ca2f67624130d
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-31 16:17:39 +0200

    initBuilt-inFunctions-11.FindAllEmployeesWithRank2

--- CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
@@ -59,3 +59,15 @@ SELECT EmployeeID, FirstName, LastName, Salary
 FROM Employees
 WHERE Salary BETWEEN 10000 AND 50000
 ORDER BY Salary DESC
+
+--problem 11. Find All Employees with Rank 2
+SELECT EmployeeID, FirstName, LastName, Salary, Rank
+FROM    (SELECT EmployeeID, FirstName, LastName, Salary
+        ,DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+        FROM Employees
+        WHERE Salary BETWEEN 10000 AND 50000) AS InnerQuery
+WHERE Rank = 2 
+ORDER BY Salary DESC
+
+
+    
