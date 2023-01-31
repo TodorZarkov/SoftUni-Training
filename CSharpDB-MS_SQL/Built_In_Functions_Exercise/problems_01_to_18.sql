4a555e02ff7420437e8fcce2e165db9bcc340f92
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-31 12:41:58 +0200

    init Built-inFunctions-02.FindNamesOfAllEmploye...

--- CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
@@ -1,5 +1,4 @@
 --problem 01. Find Names of All Employees by First Name
-
 USE SoftUni
 GO
 
@@ -7,3 +6,8 @@ SELECT FirstName, LastName
 FROM Employees
 WHERE LEFT(FirstName,2) = 'Sa'
 
+
+--problem 02. Find Names of All Employees by Last Name
+SELECT FirstName, LastName
+FROM Employees
+WHERE CHARINDEX('ei', LastName) <> 0
