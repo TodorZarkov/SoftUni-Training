ed5049fc139134c0706b02e01216e73335762eea
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-31 12:48:42 +0200

    init Built-inFunctions-03.FindFirstNamesOfAllEm...

--- CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
@@ -11,3 +11,8 @@ WHERE LEFT(FirstName,2) = 'Sa'
 SELECT FirstName, LastName
 FROM Employees
 WHERE CHARINDEX('ei', LastName) <> 0
+
+--problem 03. Find First Names of All Employees
+SELECT FirstName
+FROM Employees
+WHERE DepartmentID IN(3,10) AND YEAR(HireDate) BETWEEN 1995 AND 2005
