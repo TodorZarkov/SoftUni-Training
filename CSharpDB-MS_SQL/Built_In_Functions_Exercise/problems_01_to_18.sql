3d653cf27856fb99d7537c6fc257abbaa5a0e126
TodorZarkov <t.p.zarkov@gmail.com>
2023-01-31 18:18:01 +0200

    initBuilt-inFunctions-17.ShowAllGamesWithDurati...

--- CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Built_In_Functions_Exercise/problems_01_to_18.sql
@@ -102,3 +102,19 @@ SELECT Username, IpAddress AS [IP Address]
 FROM Users
 WHERE IpAddress LIKE '___.1_%._%.___'
 ORDER BY Username
+
+--problem 17. Show All Games with Duration & Part of the Day
+SELECT [Name] AS [Game]
+    ,CASE
+        WHEN DATEPART(HOUR,Start) BETWEEN 0 AND 11 THEN 'Morning'
+        WHEN DATEPART(HOUR,Start) BETWEEN 12 AND 17 THEN 'Afternoon'
+        WHEN DATEPART(HOUR,Start) BETWEEN 18 AND 23 THEN 'Evening'
+    END AS [Part of the Day]
+    ,CASE
+        WHEN Duration <= 3 THEN 'Extra Short'
+        WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+        WHEN Duration > 6 THEN 'Long'
+        ELSE 'Extra Long'
+    END AS [Duration]
+FROM Games
+ORDER BY Game , Duration , [Part of the Day]
