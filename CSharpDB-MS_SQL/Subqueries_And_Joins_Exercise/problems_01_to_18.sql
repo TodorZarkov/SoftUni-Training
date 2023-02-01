36b9078d622bc2385bf7fa6f7da7193c77bfd4fe
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-01 16:17:28 +0200

    init Subqueries AndJoins-06. Employees Hired After

--- CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
@@ -41,5 +41,10 @@ WHERE ep.ProjectID IS NULL
 ORDER BY e.EmployeeID
 
 --problem 06. Employees Hired After
-
+SELECT e.FirstName, e.LastName, e.HireDate, d.Name AS DeptName
+FROM Employees AS e
+JOIN Departments AS d
+ON e.DepartmentID = d.DepartmentID
+WHERE e.HireDate > '1.1.1999' AND d.Name IN('Sales' , 'Finance')
+ORDER BY e.HireDate
 
