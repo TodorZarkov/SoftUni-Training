2490cd8a88f0cf5587de8043fa9f4576bb2b0844
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-01 19:43:15 +0200

    init Subqueries AndJoins-12.HighestPeaksInBulgaria

--- CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
@@ -97,3 +97,20 @@ FROM
     )AS ms
 
 --problem 12. Highest Peaks in Bulgaria
+USE [Geography]
+GO
+SELECT 
+    c.CountryCode
+    ,m.MountainRange
+    ,p.PeakName
+    ,p.Elevation
+FROM Countries AS c
+JOIN MountainsCountries AS mc
+ON c.CountryCode = mc.CountryCode AND c.CountryCode = 'BG'
+JOIN Mountains AS m
+ON mc.MountainId = m.Id
+JOIN Peaks AS p
+ON m.Id = p.MountainId AND p.Elevation > 2835
+ORDER BY p.Elevation DESC
+
+--problem 13. Count Mountain Ranges
