dd15c1ab23ad1a84cc6812e2c40b8c8000f80466
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-01 15:08:15 +0200

    init Subqueries and Joins - 01. Employee Address

---
+++ CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
@@ -0,0 +1,10 @@
+--problem 01. Employee Address
+USE SoftUni
+GO
+SELECT TOP(5) e.EmployeeID, e.JobTitle, e.AddressID, a.AddressText
+FROM Employees AS e
+JOIN Addresses AS a
+ON a.AddressID = e.AddressID
+ORDER BY e.AddressID
+
+
