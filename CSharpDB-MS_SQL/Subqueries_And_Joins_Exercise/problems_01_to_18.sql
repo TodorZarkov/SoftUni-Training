72a08d75c1384861c6651390b33095b68e493fd6
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-02 06:04:08 +0200

    init SubqueriesAndJoins-17.HighestPeakAndLonges...

--- CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
@@ -169,3 +169,37 @@ ON c.CountryCode = mc.CountryCode
 WHERE mc.MountainId IS NULL
 
 --problem 17. Highest Peak and Longest River by Country
+SELECT TOP(5)
+    CountryName
+    ,HighestPeakElevation
+    ,LongestRiverLength
+FROM
+    (
+    SELECT 
+        c.CountryName AS CountryName
+        ,MAX(p.Elevation) AS HighestPeakElevation
+        ,MAX(r.Length) AS LongestRiverLength
+        -- ,p.Elevation
+        -- ,p.PeakName
+        -- ,r.Length
+        -- ,r.RiverName
+        ,DENSE_RANK() OVER
+        (PARTITION BY c.CountryName ORDER BY p.Elevation DESC, r.Length DESC) Rank
+    FROM Countries AS c
+    LEFT JOIN MountainsCountries AS mc
+    ON mc.CountryCode = c.CountryCode
+    LEFT JOIN Mountains AS m
+    ON mc.MountainId = m.Id
+    LEFT JOIN Peaks AS p
+    ON m.Id = p.MountainId
+    LEFT JOIN CountriesRivers AS cr
+    ON cr.CountryCode = c.CountryCode
+    LEFT JOIN Rivers AS r
+    ON cr.RiverId = r.Id
+    GROUP BY c.CountryName, p.Elevation, r.Length
+    )
+AS in1
+WHERE Rank = 1
+ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, CountryName
+
+--problem 18. Highest Peak Name and Elevation by Country
