addb36058896b1db9c5c9ad14c5a34d466664217
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-02 14:23:31 +0200

    init Ind. & Data Agg. - 02. Longest Magic Wand

--- CSharpDB-MS_SQL/Indices_And_Data_Aggregation_Exercise/problems_01_to_19.sql
+++ CSharpDB-MS_SQL/Indices_And_Data_Aggregation_Exercise/problems_01_to_19.sql
@@ -5,3 +5,7 @@ SELECT COUNT(*) AS Count
 FROM WizzardDeposits
 
 --problem 02. Longest Magic Wand
+SELECT MAX(w.MagicWandSize) AS LongestMagicWand
+FROM WizzardDeposits AS w
+
+--problem 03. Longest Magic Wand per Deposit Groups
