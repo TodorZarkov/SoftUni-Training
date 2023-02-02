54039b6237dda5cc25e3948c6352c49a9a8f0a64
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-02 06:44:03 +0200

    init SubqueriesAndJoins-18.HighestPeakNameAndEl...

--- CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
+++ CSharpDB-MS_SQL/Subqueries_And_Joins_Exercise/problems_01_to_18.sql
@@ -203,3 +203,39 @@ WHERE Rank = 1
 ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, CountryName
 
 --problem 18. Highest Peak Name and Elevation by Country
+SELECT TOP(5)
+    Country
+    ,[Highest Peak Name]
+    ,[Highest Peak Elevation]
+    ,[Mountain]
+FROM
+(
+    SELECT 
+            c.CountryName AS Country
+            ,CASE
+                WHEN MAX(p.Elevation) IS NULL  THEN 0
+                ELSE MAX(p.Elevation)
+            END AS [Highest Peak Elevation]
+            ,CASE
+                WHEN p.PeakName IS NULL THEN '(no highest peak)'
+                ELSE p.PeakName
+            END AS [Highest Peak Name]
+            ,CASE
+                WHEN m.MountainRange IS NULL THEN '(no mountain)'
+                ELSE m.MountainRange
+            END AS [Mountain]
+            ,DENSE_RANK() OVER
+            (PARTITION BY c.CountryName ORDER BY p.Elevation DESC) Rank
+        FROM Countries AS c
+        LEFT JOIN MountainsCountries AS mc
+        ON mc.CountryCode = c.CountryCode
+        LEFT JOIN Mountains AS m
+        ON mc.MountainId = m.Id
+        LEFT JOIN Peaks AS p
+        ON m.Id = p.MountainId
+        GROUP BY c.CountryName, p.Elevation, p.PeakName, m.MountainRange
+)AS in1
+WHERE Rank = 1
+ORDER BY Country, [Highest Peak Name]
+
+
