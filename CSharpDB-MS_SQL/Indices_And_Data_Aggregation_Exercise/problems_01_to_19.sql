6d5780c2de8de81fad3601d7e695bb89a41307a3
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-02 22:04:57 +0200

    init Indices and Data Aggregation - 16. Employees Maximum Salaries

--- CSharpDB-MS_SQL/Indices_And_Data_Aggregation_Exercise/problems_01_to_19.sql
+++ CSharpDB-MS_SQL/Indices_And_Data_Aggregation_Exercise/problems_01_to_19.sql
@@ -152,5 +152,12 @@ GROUP BY ne.DepartmentID
 
 
 --problem 16. Employees Maximum Salaries 
+SELECT
+    e.DepartmentID
+    ,MAX(e.Salary) AS MaxSalary
+FROM Employees AS e
+GROUP BY e.DepartmentID
+HAVING MAX(e.Salary) NOT BETWEEN 30000 AND 70000
 
 
+--problem 17. Employees Count Salaries
