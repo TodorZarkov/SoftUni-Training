d15a249eb0fd64de482e52ffece4b1e00e7b0100
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-02 21:20:07 +0200

    init Indices and Data Aggregation - 13. Departments Total Salaries

--- CSharpDB-MS_SQL/Indices_And_Data_Aggregation_Exercise/problems_01_to_19.sql
+++ CSharpDB-MS_SQL/Indices_And_Data_Aggregation_Exercise/problems_01_to_19.sql
@@ -112,3 +112,13 @@ JOIN WizzardDeposits AS wg
 ON wh.Id = (wg.Id - 1)
 
 --problem 13. Departments Total Salaries
+USE SoftUni
+GO
+SELECT
+    e.DepartmentID
+    ,SUM(e.Salary) AS TotalSalary
+FROM Employees AS e
+GROUP BY e.DepartmentID
+ORDER BY e.DepartmentID
+
+--problem 14. Employees Minimum Salaries
