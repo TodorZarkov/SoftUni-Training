199d5e81671ba63fba56a6d4d41fadad76542ea2
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-03 23:47:50 +0200

    init Func. and Stored Proc. - 04. Employees from Town

--- CSharpDB-MS_SQL/Functions_And_Stored_Procedures_Exercise/problems_01-13.sql
+++ CSharpDB-MS_SQL/Functions_And_Stored_Procedures_Exercise/problems_01-13.sql
@@ -22,8 +22,6 @@ WHERE e.Salary >= @salaryThreshold
 GO
 
 --problem 03. Town Names Starting With
-USE [SoftUni]
-GO
 CREATE PROC usp_GetTownsStartingWith(@startString NVARCHAR(MAX))
 AS
 SELECT
@@ -33,3 +31,17 @@ WHERE LEFT(t.Name,LEN(@startString)) = @startString
 GO
 
 --problem 04. Employees from Town
+CREATE PROC usp_GetEmployeesFromTown(@townName NVARCHAR(200))
+AS
+SELECT  
+    e.FirstName
+    ,e.LastName
+FROM Employees AS e
+JOIN Addresses AS a
+ON e.AddressID = a.AddressID
+JOIN Towns AS t
+ON a.TownID = t.TownID
+WHERE t.Name = @townName
+GO
+
+--problem 05. Salary Level Function
