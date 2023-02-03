4ce93828f7145390681ef06d49a09c4d64da3466
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-04 00:32:30 +0200

    init Func. and Stored Proc. - 05. Salary Level Function

--- CSharpDB-MS_SQL/Functions_And_Stored_Procedures_Exercise/problems_01-13.sql
+++ CSharpDB-MS_SQL/Functions_And_Stored_Procedures_Exercise/problems_01-13.sql
@@ -45,3 +45,19 @@ WHERE t.Name = @townName
 GO
 
 --problem 05. Salary Level Function
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR(7)
+AS
+BEGIN
+    DECLARE @result VARCHAR(7)
+    IF (@salary < 30000)
+        SET @result = 'Low'
+    ELSE IF @salary BETWEEN 30000 AND 50000
+        SET @result = 'Average'
+    ELSE
+        SET @result = 'High'
+    RETURN @result
+END
+
+--problem 06. Employees by Salary Level
+
