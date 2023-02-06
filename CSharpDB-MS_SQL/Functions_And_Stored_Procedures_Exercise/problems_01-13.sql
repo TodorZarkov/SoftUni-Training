afdf4bf915317333e165953b702e5e270d9828e3
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-06 23:37:21 +0200

    init Func. and Stored Proc. - 11. Future Value Function

--- CSharpDB-MS_SQL/Functions_And_Stored_Procedures_Exercise/problems_01-13.sql
+++ CSharpDB-MS_SQL/Functions_And_Stored_Procedures_Exercise/problems_01-13.sql
@@ -189,5 +189,17 @@ AS
 GO
 
 --problem 11. Future Value Function
+CREATE FUNCTION ufn_CalculateFutureValue(@initSum DECIMAL(38,5) ,@yearlyInterestRate FLOAT(53) , @years INT)
+RETURNS DECIMAL(38,4)
+AS
+BEGIN
+    DECLARE @futureValue DECIMAL(38,4)
+    SET @futureValue = ROUND(   @initSum * POWER((1+@yearlyInterestRate) ,@years)   ,4)
+    RETURN @futureValue
+END
+GO
 
+SELECT dbo.ufn_CalculateFutureValue(1000, 0.1, 5)
+
+--problem 12. Calculating Interest
 
