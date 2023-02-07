c6aa6aa23d03dcd9816e17bd84709f60451b9f2c
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-07 16:37:37 +0200

    init Triggers and Transactions - 02. Create Table Emails

--- CSharpDB-MS_SQL/Triggers_And_Transactions/problems_01-09.sql
+++ CSharpDB-MS_SQL/Triggers_And_Transactions/problems_01-09.sql
@@ -28,5 +28,32 @@ SELECT * FROM Logs
 UPDATE Accounts
 SET Balance = 200
 WHERE Id = 1
+GO
 
 --problem 02. Create Table Emails
+CREATE TABLE NotificationEmails
+(
+      Id BIGINT PRIMARY KEY IDENTITY(1,1)
+    , Recipient INT NOT NULL
+        ,FOREIGN KEY(Recipient) REFERENCES Accounts(Id)
+    , [Subject] VARCHAR(100) NOT NULL
+    , Body VARCHAR(300) NOT NULL
+)
+GO
+
+CREATE TRIGGER tr_Email_On_Log_Update
+ON Logs FOR INSERT
+AS
+    INSERT INTO NotificationEmails (Recipient, [Subject], Body)
+    SELECT 
+        i.AccountId
+        , CONCAT('Balance change for account: '
+        , i.AccountId)
+        , CONCAT('On ',CONVERT(varchar,GETDATE(),0),' your balance was changed from ',i.OldSum,' to ', i.NewSum,'.')
+    FROM inserted AS i
+GO
+
+SELECT * FROM NotificationEmails
+
+--problem 03. Deposit Money
+
