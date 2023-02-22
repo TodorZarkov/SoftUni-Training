0f0d3e4f7ff4758c07259c9e9e9d7be088c16682
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-22 15:26:39 +0200

    init - ado.net - P02.VillainNames

---
+++ CSharp-Entity_Framework_Core/ADO.NET-Exercise/P02.VillainNames/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace P02.VillainNames
+{
+
+    internal static class Config
+    {
+        public const string ConnectionString =
+            @"Server=.;Database=MinionsDB;User Id=SA;Password=barabinecSA!;TrustServerCertificate=true";
+    }
+}
---
+++ CSharp-Entity_Framework_Core/ADO.NET-Exercise/P02.VillainNames/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace P02.VillainNames
+{
+    using Microsoft.Data.SqlClient;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection connection =
+                new SqlConnection(Config.ConnectionString);
+            connection.Open();
+
+            Console.WriteLine("Connected to db!");
+        }
+    }
+}
