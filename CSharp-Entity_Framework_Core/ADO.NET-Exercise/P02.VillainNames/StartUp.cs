c0dfcdf66130fd1f769b5a160098e4a93457d0e6
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-23 23:01:06 +0200

    update - ado.net - P02.VillainNames

--- CSharp-Entity_Framework_Core/ADO.NET-Exercise/P02.VillainNames/StartUp.cs
+++ CSharp-Entity_Framework_Core/ADO.NET-Exercise/P02.VillainNames/StartUp.cs
@@ -6,11 +6,33 @@
     {
         static void Main(string[] args)
         {
+            string sqlQuery =
+                @"  SELECT 
+                    CONCAT_WS(' - ',v.Name, COUNT(*)) AS [Output]
+                    FROM Villains AS v
+                    JOIN MinionsVillains AS mv
+                    ON mv.VillainId = v.Id
+                    JOIN Minions AS m
+                    ON m.Id = mv.MinionId
+                    GROUP BY v.Id, v.Name
+                    HAVING COUNT(*) > @minMinions
+                    ORDER BY COUNT(*) DESC
+                ";
+            int minMinions = 3;
+
+
             using SqlConnection connection =
                 new SqlConnection(Config.ConnectionString);
-            connection.Open();
 
-            Console.WriteLine("Connected to db!");
+            SqlCommand command = new SqlCommand(sqlQuery, connection);
+            command.Parameters.AddWithValue("@minMinions", minMinions);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]);
+            }
         }
     }
 }
