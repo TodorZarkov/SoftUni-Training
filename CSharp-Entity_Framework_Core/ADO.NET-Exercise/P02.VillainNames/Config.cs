2b39e6963df93c8fc24514c06bc9992b61b5c843
TodorZarkov <106263468+TodorZarkov@users.noreply.github.com>
2023-02-23 23:03:45 +0200

    Delete Config.cs
    security issue

--- CSharp-Entity_Framework_Core/ADO.NET-Exercise/P02.VillainNames/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P02.VillainNames
-{
-
-    internal static class Config
-    {
-        public const string ConnectionString =
-            @"Server=.;Database=MinionsDB;User Id=SA;Password=barabinecSA!;TrustServerCertificate=true";
-    }
-}
