af494663e932dd73f9b39bdd80b43edbf1258501
TodorZarkov <t.p.zarkov@gmail.com>
2023-02-27 18:57:24 +0200

    update - MiniORM - DbSet implemented

--- CSharp-Entity_Framework_Core/ORM-Fundamentals-MiniORM-6.0-Exercise/MiniORM/DbSet.cs
+++ CSharp-Entity_Framework_Core/ORM-Fundamentals-MiniORM-6.0-Exercise/MiniORM/DbSet.cs
@@ -1,7 +1,84 @@
 ﻿namespace MiniORM
 {
-    public class DbSet
+    using Microsoft.Data.SqlClient;
+    using System.Collections;
+
+    public class DbSet<TEntity> : ICollection<TEntity> where TEntity : class, new()
     {
-        // TODO: Create your DbSet class here.
+        internal DbSet(IEnumerable<TEntity> entities)
+        {
+            Entities = entities.ToList();
+            ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+
+
+        internal ChangeTracker<TEntity> ChangeTracker { get; set; }
+        internal IList<TEntity> Entities { get; set; }
+        public int Count => Entities.Count;
+        public bool IsReadOnly => Entities.IsReadOnly;
+
+
+
+        public void Add(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Item cannot be null");
+            }
+            Entities.Add(entity);
+            ChangeTracker.Add(entity);
+        }
+
+        public void Clear()
+        {
+            while (Entities.Any())
+            {
+                var entity = Entities.First();
+                Remove(entity);
+            }
+        }
+
+        public bool Contains(TEntity item)
+        {
+            return Entities.Contains(item);
+        }
+
+        public void CopyTo(TEntity[] array, int arrayIndex)
+        {
+            Entities.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return Entities.GetEnumerator();
+        }
+
+        public bool Remove(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Item cannot be null");
+            }
+            bool removedSuccessfully = Entities.Remove(item);
+
+            if (removedSuccessfully)
+            {
+                ChangeTracker.Remove(item);
+            }
+            return removedSuccessfully;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Remove(entity);
+            }
+        }
     }
 }
