a07ef97707fd43a65eb4d4a2b1381566b56d269a
TodorZarkov <t.p.zarkov@gmail.com>
2023-03-01 23:01:23 +0200

    init - Entity Framework Introduction - 02. Database First

---
+++ CSharp-Entity_Framework_Core/Entity-Framework-Introduction-Exercise/EntityFrameworkIntroduction/SoftUni/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftUni;
+
+using Data;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        SoftUniContext ctx = new SoftUniContext();
+        Console.WriteLine("Connected!");
+    }
+}
