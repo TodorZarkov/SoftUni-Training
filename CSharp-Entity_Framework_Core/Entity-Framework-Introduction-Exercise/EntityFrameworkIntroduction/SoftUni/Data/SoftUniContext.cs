1c2396d9ce9ef18a64285083fb090224795b4367
TodorZarkov <t.p.zarkov@gmail.com>
2023-03-03 11:14:19 +0200

    init - Entity Framework Introduction - 03. Employees Full Information

--- CSharp-Entity_Framework_Core/Entity-Framework-Introduction-Exercise/EntityFrameworkIntroduction/SoftUni/Data/SoftUniContext.cs
+++ CSharp-Entity_Framework_Core/Entity-Framework-Introduction-Exercise/EntityFrameworkIntroduction/SoftUni/Data/SoftUniContext.cs
@@ -25,7 +25,7 @@ public class SoftUniContext : DbContext
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;User Id=SA;Password=barabinecSA!;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("");
         }
     }
 
