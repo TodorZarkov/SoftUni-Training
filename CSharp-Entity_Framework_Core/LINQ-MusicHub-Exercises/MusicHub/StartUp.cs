366228cc4955e15401770ab10473a5b249ade3c7
TodorZarkov <t.p.zarkov@gmail.com>
2023-03-08 15:15:07 +0200

    init - LINQ - 03. Songs Above Duration

--- CSharp-Entity_Framework_Core/LINQ-MusicHub-Exercises/MusicHub/StartUp.cs
+++ CSharp-Entity_Framework_Core/LINQ-MusicHub-Exercises/MusicHub/StartUp.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Text;
+    using Castle.Core.Internal;
     using Data;
     using Initializer;
     using Microsoft.EntityFrameworkCore;
@@ -16,8 +17,10 @@
             DbInitializer.ResetDatabase(context);
 
             //Test your solutions here
-            Console.WriteLine(ExportAlbumsInfo(context,9));
-            
+            //Console.WriteLine(ExportAlbumsInfo(context,9));
+
+            //Console.WriteLine(ExportSongsAboveDuration(context, 4));
+
         }
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
@@ -30,7 +33,7 @@
                 .Single()
                 .OrderByDescending(a => a.Price);
 
-            foreach (var a  in albums)
+            foreach (var a in albums)
             {
                 sb.AppendLine($"-AlbumName: {a.Name}");
                 sb.AppendLine($"-ReleaseDate: {a.ReleaseDate.ToString("MM/dd/yyyy")}");
@@ -42,7 +45,7 @@
                     .ToArray();
                 for (int i = 0; i < songs.Length; i++)
                 {
-                    sb.AppendLine($"---{'#'}{i+1}");
+                    sb.AppendLine($"---{'#'}{i + 1}");
                     sb.AppendLine($"---SongName: {songs[i].Name}");
                     sb.AppendLine($"---Price: {songs[i].Price:f2}");
                     sb.AppendLine($"---Writer: {songs[i].Writer.Name}");
@@ -54,7 +57,48 @@
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            var songs = context.Songs
+                .AsNoTracking()
+                .Where(s => s.Duration > new TimeSpan(0, 0, duration))
+                .OrderBy(s => s.Name)
+                .ThenBy(s => s.Writer.Name)
+                .Select(s => new
+                {
+                    s.Name,
+                    PerformersFirstNames = s.SongPerformers.Select(p => p.Performer.FirstName),
+                    PerformersLastNames = s.SongPerformers.Select(p => p.Performer.LastName),
+                    WriterName = s.Writer.Name,
+                    ProducerName = s.Album.Producer.Name,
+                    s.Duration
+
+                })
+                .ToArray();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < songs.Length; i++)
+            {
+                sb.AppendLine($"-Song {"#"}{i + 1}");
+                sb.AppendLine($"---SongName: {songs[i].Name}");
+                sb.AppendLine($"---Writer: {songs[i].WriterName}");
+                if (!songs[i].PerformersFirstNames.IsNullOrEmpty())
+                {
+                    string[] firstNames = songs[i].PerformersFirstNames.ToArray();
+                    string[] lastNames = songs[i].PerformersLastNames.ToArray();
+                    string[] names = new string[firstNames.Length];
+                    for (int j = 0; j < firstNames.Length; j++)
+                    {
+                        names[j] = $"{firstNames[j]} {lastNames[j]}";
+                    }
+                    var performersNames = names.OrderBy(n => n);
+                    foreach (var n in performersNames)
+                    {
+                        sb.AppendLine($"---Performer: {n}");
+                    }
+                }
+                sb.AppendLine($"---AlbumProducer: {songs[i].ProducerName}");
+                sb.AppendLine($"---Duration: {songs[i].Duration.ToString("c")}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
