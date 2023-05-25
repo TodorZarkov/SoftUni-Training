6bd77616f3c1a073a3fb74d535c7a536189baf6d
TodorZarkov <t.p.zarkov@gmail.com>
2023-05-25 15:54:09 +0300

    init - AlgorithmsFund - 02. Recursive Drawing

---
+++ Algorithms Fundamentals with CSharp/RecursionAndBacktracking-Lab/-02.RecursiveDrawing/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.RecursiveDrawing
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+
+            Draw(row);
+        }
+
+        private static void Draw(int row)
+        {
+            if (row == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', row));
+
+            Draw(row - 1);
+
+            Console.WriteLine(new string('#', row));
+        }
+    }
+}
