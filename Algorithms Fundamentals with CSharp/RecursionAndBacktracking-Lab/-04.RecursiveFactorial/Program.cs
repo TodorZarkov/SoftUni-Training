a0938aaf009e17eea31c391e56e1aa232bfea84f
TodorZarkov <t.p.zarkov@gmail.com>
2023-05-25 16:48:22 +0300

    init - AlgorithmsFund -  04. Recursive Factorial

---
+++ Algorithms Fundamentals with CSharp/RecursionAndBacktracking-Lab/-04.RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04.RecursiveFactorial
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong n = ulong.Parse(Console.ReadLine());
+
+            ulong result =  RecursiveFactorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        private static ulong RecursiveFactorial(ulong n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * RecursiveFactorial(n - 1);
+        }
+    }
+}
