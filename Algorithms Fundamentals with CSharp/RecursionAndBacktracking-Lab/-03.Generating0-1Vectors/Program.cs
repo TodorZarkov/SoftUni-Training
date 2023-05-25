72a3e0f89999656773b9d643d1e15f7858124e7f
TodorZarkov <t.p.zarkov@gmail.com>
2023-05-25 16:25:26 +0300

    init - AlgorithmsFund - 03. Generating 0/1 Vectors

---
+++ Algorithms Fundamentals with CSharp/RecursionAndBacktracking-Lab/-03.Generating0-1Vectors/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03.Generating0_1Vectors
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int bits = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[bits];
+
+            GenerateVector(vector);
+        }
+
+        private static void GenerateVector(int[] vector, int index = 0)
+        {
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (index >= vector.Length)
+                {
+                    Console.WriteLine(string.Join("", vector));
+                    return;
+                }
+                vector[index] = i;
+                GenerateVector(vector, index + 1);
+            }
+        }
+    }
+}
