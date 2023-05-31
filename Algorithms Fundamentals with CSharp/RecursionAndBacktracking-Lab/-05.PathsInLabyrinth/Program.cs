48a01fe8e2617659f58cf819f051e69230a68160
TodorZarkov <t.p.zarkov@gmail.com>
2023-05-31 18:02:03 +0300

    update - algorithms - labirinth path

--- Algorithms Fundamentals with CSharp/RecursionAndBacktracking-Lab/-05.PathsInLabyrinth/Program.cs
+++ Algorithms Fundamentals with CSharp/RecursionAndBacktracking-Lab/-05.PathsInLabyrinth/Program.cs
@@ -1,32 +1,69 @@
 ﻿namespace _05.PathsInLabyrinth
 {
-    using System;
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            int height = int.Parse(Console.ReadLine());
-            int width = int.Parse(Console.ReadLine());
+	using System;
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int height = int.Parse(Console.ReadLine());
+			int width = int.Parse(Console.ReadLine());
 
-            char[][] lab = new char[height][];
-            for (int i = 0; i < height; i++)
-            {
-                lab[i] = Console.ReadLine().ToArray();
-            }
+			char[][] lab = new char[height][];
+			for (int i = 0; i < height; i++)
+			{
+				lab[i] = Console.ReadLine().ToCharArray();
+			}
 
-            PrintLabyrinth(lab);
-        }
+			Stack<string> path = new Stack<string>();
 
-        private static void PrintLabyrinth(char[][] lab)
-        {
-            for (int i = 0; i < lab.GetLength(0); i++)
-            {
-                for (int j = 0; j < lab.GetLength(1); j++)
-                {
-                    Console.WriteLine(lab[i][j]);
-                }
-                Console.WriteLine();
-            }
-        }
-    }
+			GoNext(0, 0, "", lab, path);
+
+		}
+
+		private static void GoNext(int x, int y, string step, char[][] lab, Stack<string> path)
+		{
+			if (x < 0 || y < 0 || x > lab.Length - 1 || y > lab[x].Length - 1)
+			{
+				return;
+			}
+
+			if (lab[x][y] == 'e')
+			{
+				Console.WriteLine(string.Join("", path));
+				return;
+			}
+
+			if (lab[x][y] == 'v')
+			{
+				return;
+			}
+
+			if (lab[x][y] == '*')
+			{
+				return;
+			}
+
+			lab[x][y] = 'v';
+			path.Push(step);
+
+			GoNext(x + 1, y, "R", lab, path); //right
+			GoNext(x - 1, y, "L", lab, path); //left
+			GoNext(x, y + 1, "U", lab, path); //up
+			GoNext(x, y - 1, "D", lab, path); //down
+
+			if (lab[x][y] == 'v')
+			{
+				lab[x][y] = '-';
+				path.Pop();
+			}
+		}
+
+		private static void PrintLabyrinth(char[][] lab)
+		{
+			for (int i = 0; i < lab.Length; i++)
+			{
+				Console.WriteLine(string.Join("", lab[i]));
+			}
+		}
+	}
 }
