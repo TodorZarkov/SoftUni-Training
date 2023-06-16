f646f4195c2cdcc7f39555fb90768161e607f212
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-16 10:22:05 +0300

    update - asp.net - LibraryWorkshop - Scaffold identity, Login, Logout

--- ASP.NET Fundamentals/Library-Workshop/Library/Controllers/HomeController.cs
+++ ASP.NET Fundamentals/Library-Workshop/Library/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Library.Models;
+﻿using Library.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
--- ASP.NET Fundamentals/Library-Workshop/Library/Program.cs
+++ ASP.NET Fundamentals/Library-Workshop/Library/Program.cs
@@ -7,11 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<LibraryDbContext>(options =>
-    options.UseSqlServer(connectionString));
+	options.UseSqlServer(connectionString));
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<LibraryDbContext>();
+builder.Services
+	.AddDefaultIdentity<IdentityUser>(options =>
+	{
+		options.SignIn.RequireConfirmedAccount = false;
+		options.Password.RequireDigit = false;
+		options.Password.RequireNonAlphanumeric = false;
+		options.Password.RequireUppercase = false;
+	})
+	.AddEntityFrameworkStores<LibraryDbContext>();
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -19,13 +30,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+	app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -37,8 +48,8 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
