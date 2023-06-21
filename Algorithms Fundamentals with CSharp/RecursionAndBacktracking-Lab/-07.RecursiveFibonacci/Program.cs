8170a236eb8b9a4483204da2022c9d4092e173d5
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-21 16:19:16 +0300

    update - Algorithms - 07. Recursive Fibonacci with memorization and longn overflow exception

--- Algorithms Fundamentals with CSharp/RecursionAndBacktracking-Lab/-07.RecursiveFibonacci/Program.cs
+++ Algorithms Fundamentals with CSharp/RecursionAndBacktracking-Lab/-07.RecursiveFibonacci/Program.cs
@@ -1,12 +1,36 @@
 ﻿namespace _07.RecursiveFibonacci
 {
     using System;
+    using System.Collections.Generic;
 
     public class Program
     {
+        static Dictionary<int,long> fibonaccies = new Dictionary<int, long>();
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            long result = GetFibonacci(n);
+            Console.WriteLine(result);
+        }
+
+        private static long GetFibonacci(int n)
+        {
+            if (n > 91)
+            {
+                throw new OverflowException($"Reached long max value (${long.MaxValue}");
+            }
+            if (n < 2)
+            {
+                return 1;
+            }
+            if (fibonaccies.ContainsKey(n))
+            {
+                return fibonaccies[n];
+            }
+            long result = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            fibonaccies[n] = result;
             
+            return result;
         }
     }
 }
