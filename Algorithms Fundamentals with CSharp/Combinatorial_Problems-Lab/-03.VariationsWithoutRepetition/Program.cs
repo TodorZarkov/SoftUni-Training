10ea0da6ac808411c13ed832f8bf24f28a2f9d0c
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-22 08:44:18 +0300

    init - Algorithms - 03. Variations without Repetition

---
+++ Algorithms Fundamentals with CSharp/Combinatorial_Problems-Lab/-03.VariationsWithoutRepetition/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _03.VariationsWithoutRepetition
+{
+    using System;
+    using System.Linq;
+
+
+    public class Program
+    {
+        private static string[] variations;
+
+        private static string[] elements;
+
+        private static bool[] used;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(" ").ToArray();
+            int k = int.Parse(Console.ReadLine());
+            variations = new string[k];
+            used = new bool[elements.Length];
+            Variate(0);
+        }
+
+        private static void Variate(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Print();
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        variations[index] = elements[i];
+                        Variate(index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+        private static void Print()
+        {
+            Console.WriteLine(string.Join(" ", variations));
+        }
+    }
+}
