21735216b25d1f763a8030c4e830e9389f462e0c
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-22 14:51:42 +0300

    init - Algorithms - 06. Combinations with Repetition

---
+++ Algorithms Fundamentals with CSharp/Combinatorial_Problems-Lab/-06.CombinationsWithRepetition/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.CombinationsWithRepetition
+{
+    using System;
+    using System.Linq;
+
+
+    public class Program
+    {
+        private static int k;
+
+        private static string[] combinations;
+
+        private static string[] elements;
+
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(" ").ToArray();
+            k = int.Parse(Console.ReadLine());
+            combinations = new string[k];
+
+            Combinate(0, 0);
+        }
+
+        private static void Combinate(int index, int elementStartIdx)
+        {
+            if (index >= combinations.Length)
+            {
+                Print();
+                return;
+            }
+
+            for (int i = elementStartIdx; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combinate(index + 1, i);
+            }
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine(string.Join(" ", combinations));
+        }
+
+    }
+}
