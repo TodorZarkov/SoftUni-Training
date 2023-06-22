d641ef23fe1d8d10ff4048424c19e39396083865
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-22 15:50:39 +0300

    init - Algorithms - Recursion and Combinatorial Problems - 01. Reverse Array

---
+++ Algorithms Fundamentals with CSharp/RecursionAndCombinatorialProblems-Exercise/-01.ReverseArray/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.ReverseArray
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static int[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(" ").Select(e => int.Parse(e)).ToArray();
+
+            Reverse(0);
+        }
+
+        private static void Reverse(int index)
+        {
+            if (index >= elements.Length/2)
+            {
+                Console.WriteLine(string.Join(" ",elements));
+                return;
+            }
+            (elements[index], elements[elements.Length - 1 - index]) = 
+                (elements[elements.Length - 1 - index], elements[index]);
+            Reverse(index + 1);
+        }
+    }
+}
