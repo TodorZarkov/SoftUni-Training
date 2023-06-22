798ef8f7ca2ddc270e007d78a4020c03a87161df
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-22 07:10:09 +0300

    init - Algorithms - 01. Permutations without Repetition (with swap)

---
+++ Algorithms Fundamentals with CSharp/Combinatorial_Problems-Lab/-01.PermutationsWithoutRepetition/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _01.PermutationsWithoutRepetition
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        private static string[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(" ").ToArray();
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Print();
+            } 
+            else
+            {
+                Permute(index + 1);
+                for (int i = index + 1; i < elements.Length; i++)
+                {
+                    Swap(index, i);
+                    Permute(index + 1);
+                    Swap(index, i);
+                }
+            }
+        }
+
+        private static void Swap(int index, int i)
+        {
+            (elements[i], elements[index]) = (elements[index], elements[i]);
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
