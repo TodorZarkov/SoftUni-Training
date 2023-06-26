4cf4ab88b5f103ccf2f8e1c976c3ea68595bbc1d
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-26 16:50:57 +0300

    init - Algorithms - Searching, Sorting and Greedy Algorithms - 01. Binary Search

---
+++ Algorithms Fundamentals with CSharp/RecursionAndCombinatorialProblems-Exercise/-06.WordCruncher-ValidPlaceApprch/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _06.WordCruncher_ValidPlaceApprch
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Approach where you get the possible words and place them to the only places they can be.
+        }
+    }
+}
---
+++ Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/01.BinarySearch/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01.BinarySearch
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sortedArray = Console.ReadLine()
+                .Split(" ")
+                .Select(e => int.Parse(e))
+                .ToArray();
+
+            int searchFor = int.Parse(Console.ReadLine());
+
+            int result = BinarySearch(sortedArray, searchFor);
+
+            Console.WriteLine(result);
+        }
+
+        private static int BinarySearch(int[] sortedArray, int searchFor)
+        {
+            int left = 0;
+            int right = sortedArray.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (right + left) / 2;
+                if (sortedArray[mid] == searchFor)
+                {
+                    return mid;
+                }
+                else if (sortedArray[mid] > searchFor)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid  + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
