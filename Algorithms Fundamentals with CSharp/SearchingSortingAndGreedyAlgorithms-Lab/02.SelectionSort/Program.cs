050722d87ad764d082a11206e36ae06189b9d63f
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-27 09:15:19 +0300

    init - Algorithms - Searching, Sorting and Greedy Algorithms - 02. Selection Sort

---
+++ Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/02.SelectionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02.SelectionSort
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(n => int.Parse(n))
+                .ToArray();
+
+            SelectionSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void SelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[i])
+                    {
+                        (numbers[i], numbers[j]) =
+                            (numbers[j], numbers[i]);
+                    }
+                }
+            }
+        }
+
+    }
+}
