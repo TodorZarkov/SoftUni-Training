f0944a038b922babe241371e0b2416079ff93fd9
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-27 12:13:43 +0300

    init - Algorithms - Searching, Sorting and Greedy Algorithms - 04. Insertion Sort

---
+++ Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/04.InsertionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.InsertionSort
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(n => int.Parse(n))
+                .ToArray();
+
+            InsertionSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void InsertionSort(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int j = i - 1;
+                int min = i;
+                while (j >= 0 && numbers[min] < numbers[j])
+                {
+                    (numbers[min], numbers[j]) = (numbers[j], numbers[min]);
+                    min = j;
+                    j--;
+                }
+            }
+        }
+    }
+}
