febb2b75e73299a9cfc3c111bba43fd5d3df0243
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-27 11:03:17 +0300

    init - Algorithms - Searching, Sorting and Greedy Algorithms - 03. Bubble Sort

---
+++ Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/03.BubbleSort/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03.BubbleSort
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(n => int.Parse(n))
+                .ToArray();
+
+            BubbleSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            bool hasChanged = false;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                        hasChanged = true;
+                    }
+                }
+                if (!hasChanged)
+                {
+                    break;
+                }
+                hasChanged = false;
+            }
+        }
+    }
+}
