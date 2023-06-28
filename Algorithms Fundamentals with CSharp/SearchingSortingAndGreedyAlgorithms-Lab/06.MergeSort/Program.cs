5418098a12f9365e4bba3e10210548101a564233
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-28 12:30:49 +0300

    init - Algorithms - Searching, Sorting and Greedy Algorithms - 06. MergeSort

---
+++ Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/06.MergeSort/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.MergeSort
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(n => int.Parse(n))
+                .ToArray();
+
+            MergeSort(numbers, 0, numbers.Length - 1);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void MergeSort(int[] numbers, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int leftStart = start;
+            int leftEnd = (start + end) / 2;
+            int rightStart = ((start + end) / 2) +1;
+            int rightEnd = end;
+            MergeSort(numbers, leftStart, leftEnd);
+            MergeSort(numbers, rightStart, rightEnd);
+        }
+    }
+}
