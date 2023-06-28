9e62c72a7d3b9b3deca6e0c9e9a7e3c602f7dc8b
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-29 02:20:12 +0300

    update - Algorithms - Searching, Sorting and Greedy Algorithms - 06. MergeSort

--- Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/06.MergeSort/Program.cs
+++ Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/06.MergeSort/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace _06.MergeSort
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     internal class Program
@@ -11,6 +12,7 @@
                 .Split(" ")
                 .Select(n => int.Parse(n))
                 .ToArray();
+
 
             MergeSort(numbers, 0, numbers.Length - 1);
 
@@ -25,10 +27,48 @@
             }
             int leftStart = start;
             int leftEnd = (start + end) / 2;
-            int rightStart = ((start + end) / 2) +1;
+            int rightStart = ((start + end) / 2) + 1;
             int rightEnd = end;
+
             MergeSort(numbers, leftStart, leftEnd);
             MergeSort(numbers, rightStart, rightEnd);
+
+            int left = leftStart;
+            int right = rightStart;
+            List<int> helpList = new List<int>(rightEnd - leftStart + 1);
+            while (left <= leftEnd && right <= rightEnd)
+            {
+                if (numbers[left] > numbers[right])
+                {
+                    // insert before left
+                    helpList.Add(numbers[right]);
+                    right++;
+
+                }
+                else if (numbers[left] <= numbers[right])
+                {
+                    //insert after left
+                    helpList.Add(numbers[left]);
+                    left++;
+
+                }
+
+            }
+            for (int i = left; i <= leftEnd; i++)
+            {
+                helpList.Add(numbers[i]);
+            }
+            for (int i = right; i <= rightEnd; i++)
+            {
+                helpList.Add(numbers[i]);
+            }
+
+            helpList.CopyTo(numbers, leftStart);
+        }
+
+        private static void Swap(List<int> helpList, int left, int right)
+        {
+            (helpList[left], helpList[right]) = (helpList[right], helpList[left]);
         }
     }
 }
