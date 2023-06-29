c3e97a6f128faa445de38ec6c12cbbd02965f5fe
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-29 03:16:44 +0300

    init - Algorithms - Searching, Sorting and Greedy Algorithms - 08. Set Cover

---
+++ Algorithms Fundamentals with CSharp/SearchingSortingAndGreedyAlgorithms-Lab/08.SetCover/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _08.SetCover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var universe = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToHashSet();
+
+            var n = int.Parse(Console.ReadLine());
+
+            var sets = new List<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var set = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                sets.Add(set);
+            }
+
+            var selectedSets = new List<int[]>();
+            while (universe.Count > 0)
+            {
+                var set = sets
+                    .OrderByDescending(s => s.Count(e => universe.Contains(e)))
+                    .FirstOrDefault();
+
+                selectedSets.Add(set);
+
+                sets.Remove(set);
+
+                foreach (var item in set)
+                {
+                    universe.Remove(item);
+                }
+            }
+
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+            foreach (var item in selectedSets)
+            {
+                Console.WriteLine(string.Join(", ",  item));
+            }
+        }
+    }
+}
