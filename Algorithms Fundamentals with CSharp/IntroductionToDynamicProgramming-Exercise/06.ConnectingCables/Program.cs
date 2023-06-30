c6ed18e4f87074bf8389685d73b48eece32efbbf
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-30 17:30:12 +0300

    init - Introduction to Dynamic Programming - Exercise  -   06. Connecting Cables

---
+++ Algorithms Fundamentals with CSharp/IntroductionToDynamicProgramming-Exercise/06.ConnectingCables/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _06.ConnectingCables
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var positions = Enumerable.Range(1, numbers.Length).ToArray();
+
+            var dp = new int[numbers.Length + 1, positions.Length + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                for (int col = 1; col < dp.GetLength(1); col++)
+                {
+                    if (numbers[row - 1] == positions[col - 1])
+                    {
+                        dp[row, col] = dp[row - 1, col - 1] + 1;
+                    }
+                    else
+                    {
+                        dp[row, col] = Math.Max(dp[row - 1, col], dp[row, col - 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Maximum pairs connected: {dp[numbers.Length, numbers.Length]}");
+        }
+    }
+}
