5dee4b1614b9d6d63dc10ad21bd44560a58c78e7
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-30 17:19:55 +0300

    init - Introduction to Dynamic Programming - Exercise  -     05. Word Differences

---
+++ Algorithms Fundamentals with CSharp/IntroductionToDynamicProgramming-Exercise/05.WordDifferences/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _05.WordDifferences
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            var dp = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                dp[row, 0] = row;
+            }
+
+            for (int col = 1; col < dp.GetLength(1); col++)
+            {
+                dp[0, col] = col;
+            }
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                for (int col = 1; col < dp.GetLength(1); col++)
+                {
+                    if (str1[row - 1] == str2[col - 1])
+                    {
+                        dp[row, col] = dp[row - 1, col - 1];
+                    }
+                    else
+                    {
+                        dp[row, col] = Math.Min(dp[row - 1, col], dp[row, col - 1]) + 1;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Deletions and Insertions: {dp[str1.Length, str2.Length]}");
+        }
+    }
+}
