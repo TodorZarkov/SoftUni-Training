a151869d630de1b8a1e0a480eb91e3839b747d16
TodorZarkov <t.p.zarkov@gmail.com>
2023-07-01 02:23:08 +0300

    init - Algorithms Fundamentals - Exam Preparation  -    01. Trains

---
+++ Algorithms Fundamentals with CSharp/ExamPreparation-24July2022/01.Trains/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01.Trains
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var  arrival = Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .OrderBy(a => a)
+            .ToArray();
+
+           var departure = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .OrderBy(d => d)
+                .ToArray();
+
+            var platforms = 0;
+            var currentPlatforms = 0;
+            var arvIndex = 0;
+            var dprIndex = 0;
+            while (arvIndex < arrival.Length && dprIndex < arrival.Length)
+            {
+                if (arrival[arvIndex] < departure[dprIndex])
+                {
+                    currentPlatforms++;
+                    arvIndex++;
+                    platforms = Math.Max(currentPlatforms, platforms);
+                }
+                else
+                {
+                    currentPlatforms--;
+                    dprIndex++;
+                }
+            }
+
+            Console.WriteLine(platforms);
+        }
+
+       
+    }
+}
