991866591e644fc43901e14b157d7cde451efdd7
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-30 16:51:45 +0300

    init - Introduction to Dynamic Programming - Exercise  -   03. Sum with Unlimited Coins

---
+++ Algorithms Fundamentals with CSharp/IntroductionToDynamicProgramming-Exercise/03.SumWithUnlimitedCoins/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.SumWithUnlimitedCoins
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountSums(numbers, target));
+        }
+
+        private static int CountSums(int[] numbers, int target)
+        {
+            var sums = new int[target + 1];
+
+            sums[0] = 1;
+
+            foreach (var number in numbers)
+            {
+                for (int sum = number; sum <= target; sum++)
+                {
+                    sums[sum] += sums[sum - number];
+                }
+            }
+
+            return sums[target];
+        }
+    }
+}
