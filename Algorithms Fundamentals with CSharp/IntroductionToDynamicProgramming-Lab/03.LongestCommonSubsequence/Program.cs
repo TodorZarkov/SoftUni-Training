1bf058e13c8b46427cbc7311f650356f48aabea9
TodorZarkov <t.p.zarkov@gmail.com>
2023-06-30 16:01:14 +0300

    init - Introduction to Dynamic Programming - Lab  -            03. Longest Common Subsequence

--- Algorithms Fundamentals with CSharp/IntroductionToDynamicProgramming-Lab/03.LongestCommonSubsequence/Program.cs
+++ Algorithms Fundamentals with CSharp/IntroductionToDynamicProgramming-Lab/03.LongestCommonSubsequence/Program.cs
@@ -6,7 +6,27 @@
     {
         static void Main(string[] args)
         {
-           
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            var lcs = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int r = 1; r < lcs.GetLength(0); r++)
+            {
+                for (int c = 1; c < lcs.GetLength(1); c++)
+                {
+                    if (str1[r-1] == str2[c-1])
+                    {
+                        lcs[r, c] = lcs[r - 1, c - 1] + 1;
+                    }
+                    else
+                    {
+                        lcs[r, c] = Math.Max(lcs[r, c - 1], lcs[r - 1, c]);
+                    }
+                }
+            }
+
+            Console.WriteLine(lcs[str1.Length, str2.Length]);
         }
     }
 }
