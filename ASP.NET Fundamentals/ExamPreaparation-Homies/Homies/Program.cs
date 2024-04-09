83141f7fe39014c8fa0f7fd08adef5685e4d3f41
TodorZarkov <t.p.zarkov@gmail.com>
2024-04-10 01:09:59 +0300

    fixing formatting

--- ASP.NET Fundamentals/ExamPreaparation-Homies/Homies/Program.cs
+++ ASP.NET Fundamentals/ExamPreaparation-Homies/Homies/Program.cs
@@ -22,7 +22,19 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 		.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
 	options.Password.RequireUppercase = builder.Configuration
 		.GetValue<bool>("Identity:Password:RequireUppercase");
-	
+
+	//options.Password.RequireLowercase = builder.Configuration
+	//    .GetValue<bool>("Identity:Password:RequireLowercase");
+	//options.Password.RequiredLength = builder.Configuration
+	//    .GetValue<int>("Identity:Password:RequiredLength");
+	//options.Lockout.MaxFailedAccessAttempts = builder.Configuration
+	//    .GetValue<int>("Identity:Lockout:MaxFailedAccessAttempts");
+
+	//options.SignIn.RequireConfirmedAccount = false;
+	//options.Password.RequireDigit = false;
+	//options.Password.RequireNonAlphanumeric = false;
+	//options.Password.RequireUppercase = false;
+
 })
     .AddEntityFrameworkStores<HomiesDbContext>();
 builder.Services.AddControllersWithViews();
